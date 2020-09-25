f839c1fc1eebc762e33c3ecfa7309d1cdcdd1bc8
Sunitha Harish <sunharis@in.ibm.com>
2020-09-25 05:45:40 -0500

    Broadcast message exchange for IBM console management
    This commit implements sending the broadcast message via
    BMC from one IBM managemenet console to other IBM management
    console
    
    Tested by:
      1. Send broadcast message
         POST https://${bmc}/ibm/v1/HMC/BroadcastService -d '{"Message":"Test broadcast message"}'
      2. Verify the message "Test broadcast message" is received at the
         IBM management console
    
    Signed-off-by: Sunitha Harish <sunharis@in.ibm.com>
    Change-Id: I4bbfbe93520e22010fd5f7189f4367338b12f15c
    
    Conflicts:
    	redfish-core/include/event_service_manager.hpp
    	redfish-core/lib/event_service.hpp

--- redfish-core/lib/event_service.hpp
+++ redfish-core/lib/event_service.hpp
@@ -28,7 +28,7 @@ static constexpr const std::array<const char*, 3> supportedRetryPolicies = {
 
 #ifdef BMCWEB_ENABLE_IBM_MANAGEMENT_CONSOLE
 static constexpr const std::array<const char*, 4> supportedResourceTypes = {
-    "IBMConfigFile", "Task", "ComputerSystem", "Manager"};
+    "IBMConfigFile", "Task", "BroadcastService", "ComputerSystem", "Manager"};
 #else
 static constexpr const std::array<const char*, 3> supportedResourceTypes = {
     "Task", "ComputerSystem", "Manager"};
