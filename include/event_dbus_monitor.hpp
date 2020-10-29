ea1aad6e4179603aaf031dc7765dc069d504eab6
Manojkiran Eda <manojkiran.eda@gmail.com>
2020-10-29 10:56:44 +0530

    Event Support for VMI IP Configuration
    - This commit would being in the eventing support for VMI hot update
      of network attributes.
    
    - This is again a GHE only commit & will be changed as per the community
      direction.
    
    Tested By:
    ===========
    
    1. Subscribe to the events on EthernetInterface using redfish listner.
    
    RegistryPrefixes:  [""]
    ResourceTypes:  ["EthernetInterface"]
    Connection
    Keep-Alive
    [{'Connection': 'Keep-Alive'}]
    HttpHeaders:  [{"Connection": "Keep-Alive"}]
    
    2. Make sure the listener gets the event when VMI configures the ip address.
    
    Subcription is successful for https://9.5.180.233
    
    Continuing with Listener.
    headers:  IOrderedDict([('Host', '9.3.84.152'), ('Content-Type', 'application/json'), ('Content-Length', '419')])
    
    bodydata:  {"@odata.type":"#Event.v1_4_0.Event","Events":[{"EventId":1,"EventTimestamp":"2020-10-29T04:12:52+00:00","EventType":"ResourceChanged","MemberId":0,"Message":"One or more resource properties have changed.","MessageArgs":null,"MessageId":"ResourceEvent.1.0.3.ResourceChanged","MessageSeverity":"OK","OriginOfCondition":"/redfish/v1/Systems/hypervisor/EthernetInterfaces/eth0","Severity":"OK"}],"Id":2,"Name":"Event Log"}
    
    Signed-off-by: Manojkiran Eda <manojkiran.eda@gmail.com>

--- include/event_dbus_monitor.hpp
+++ include/event_dbus_monitor.hpp
@@ -1,3 +1,4 @@
+#include <dbus_utility.hpp>
 #include <error_messages.hpp>
 #include <event_service_manager.hpp>
 #include <resource_messages.hpp>
@@ -9,9 +10,69 @@ namespace dbus_monitor
 
 static std::shared_ptr<sdbusplus::bus::match::match> matchHostStateChange;
 static std::shared_ptr<sdbusplus::bus::match::match> matchBMCStateChange;
+static std::shared_ptr<sdbusplus::bus::match::match> matchVMIIPStateChange;
 
 void registerHostStateChangeSignal();
 void registerBMCStateChangeSignal();
+void registerVMIIPChangeSignal();
+
+inline void VMIIPChange(sdbusplus::message::message& msg)
+{
+    BMCWEB_LOG_DEBUG << "BMC Hypervisor IP change match fired";
+
+    if (msg.is_method_error())
+    {
+        BMCWEB_LOG_ERROR << "BMC Hypervisor IP property changed Signal error";
+        return;
+    }
+
+    BMCWEB_LOG_DEBUG << msg.get_path();
+
+    std::string infname;
+    dbus::utility::getNthStringFromPath(msg.get_path(), 4, infname);
+    BMCWEB_LOG_DEBUG << infname;
+
+    boost::container::flat_map<std::string,
+                               std::variant<std::string, uint8_t, bool>>
+        values;
+    std::string objName;
+    msg.read(objName, values);
+    BMCWEB_LOG_DEBUG << objName;
+
+    auto find = values.find("Enabled");
+    if (find == values.end())
+    {
+        BMCWEB_LOG_ERROR << "Enabled property not Found";
+        return;
+    }
+    BMCWEB_LOG_DEBUG << "Enabled property found";
+    BMCWEB_LOG_DEBUG << find->first;
+
+    const bool* propValue = std::get_if<bool>(&(find->second));
+    if (propValue != nullptr)
+    {
+        BMCWEB_LOG_DEBUG << *propValue;
+
+        if (*propValue)
+        {
+            // pldm recieves a sensor event from the hypervisor when the vmi is
+            // completely configured with an IP Address, post reciveing the
+            // sensor event, pldm will sent the Enable property to True.
+            //
+            // HMC is only interested in false -> true transition of Enabled
+            // property as that is when vmi is configured.
+            //
+            // Push an event
+            std::string origin =
+                "/redfish/v1/Systems/hypervisor/EthernetInterfaces/" + infname;
+            BMCWEB_LOG_DEBUG << "Pushing the VMI IP change Event with origin : "
+                             << origin;
+            redfish::EventServiceManager::getInstance().sendEvent(
+                redfish::messages::resourceChanged(), origin,
+                "EthernetInterface");
+        }
+    }
+}
 
 inline void BMCStatePropertyChange(sdbusplus::message::message& msg)
 {
@@ -102,10 +163,23 @@ void registerBMCStateChangeSignal()
         BMCStatePropertyChange);
 }
 
+void registerVMIIPChangeSignal()
+{
+
+    BMCWEB_LOG_DEBUG << "VMI IP change signal match - Registered";
+
+    matchVMIIPStateChange = std::make_unique<sdbusplus::bus::match::match>(
+        *crow::connections::systemBus,
+        "type='signal',member='PropertiesChanged',interface='org.freedesktop."
+        "DBus.Properties',arg0namespace='xyz.openbmc_project.Object.Enable'",
+        VMIIPChange);
+}
+
 void registerStateChangeSignal()
 {
     registerHostStateChangeSignal();
     registerBMCStateChangeSignal();
+    registerVMIIPChangeSignal();
 }
 
 } // namespace dbus_monitor
