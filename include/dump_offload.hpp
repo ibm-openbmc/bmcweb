749ad83809932d8b0cb32b7f3c666ecbaad9c0fa
Ravi Teja <raviteja28031990@gmail.com>
2020-11-08 11:02:38 -0600

    Add Dump offload support using http dynamic response
    This commit
    1.Enables dynamic response support
    2.Defines new bmcweb rule to support dump streaming.
    3.Implemented routes for bmc and system dump offload.
    
    Tested By:
    1. BMC dump offload
    2. System dump offload
    
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>
    Change-Id: I3a072d255660ee8657253430b5fadbf9a7a26b8e
    
    Conflicts:
    	include/dump_offload.hpp
    	src/webserver_main.cpp

--- include/dump_offload.hpp
+++ include/dump_offload.hpp
@@ -1,58 +1,97 @@
 #pragma once
 
-#include <signal.h>
 #include <sys/select.h>
 
+#include <boost/asio.hpp>
+#include <boost/asio/basic_socket_acceptor.hpp>
+#include <boost/asio/basic_stream_socket.hpp>
+#include <boost/asio/local/stream_protocol.hpp>
 #include <boost/beast/core/flat_static_buffer.hpp>
 #include <boost/beast/http.hpp>
-#include <boost/process.hpp>
-
-#include <cstdio>
-#include <cstdlib>
+#include <http_stream.hpp>
 
 namespace crow
 {
 namespace obmc_dump
 {
 
+using boost::asio::local::stream_protocol;
+
+std::string unixSocketPathDir = "/var/lib/bmcweb/";
+
 inline void handleDumpOffloadUrl(const crow::Request& req, crow::Response& res,
                                  const std::string& entryId,
                                  const std::string& dumpEntryType);
 inline void resetHandler();
 
-// The max network block device buffer size is 128kb plus 16bytes
-// for the message header
-static constexpr auto nbdBufferSize = 131088;
+static constexpr size_t socketBufferSize = 64 * 1024;
+static constexpr uint8_t maxConnectRetryCount = 3;
 
 /** class Handler
- *  handles data transfer between nbd-client and nbd-server.
- *  This handler invokes nbd-proxy and reads data from socket
- *  and writes on to nbd-client and vice-versa
+ *  Handles data transfer between unix domain socket and http connection socket.
+ *  This handler invokes dump offload reads data from unix domain socket
+ *  and writes on to http stream connection socket.
  */
 class Handler : public std::enable_shared_from_this<Handler>
 {
   public:
-    Handler(const std::string& mediaIn, boost::asio::io_context& ios,
-            const std::string& entryIDIn, const std::string& dumpTypeIn) :
-        pipeOut(ios),
-        pipeIn(ios), media(mediaIn), entryID(entryIDIn), dumpType(dumpTypeIn),
-        doingWrite(false), negotiationDone(false), writeonnbd(false),
-        outputBuffer(std::make_unique<
-                     boost::beast::flat_static_buffer<nbdBufferSize>>()),
-        inputBuffer(
-            std::make_unique<boost::beast::flat_static_buffer<nbdBufferSize>>())
-    {}
-
-    ~Handler()
+    Handler(boost::asio::io_context& ios, const std::string& entryIDIn,
+            const std::string& dumpTypeIn,
+            const std::string& unixSocketPathIn) :
+        entryID(entryIDIn),
+        dumpType(dumpTypeIn),
+        outputBuffer(boost::beast::flat_static_buffer<socketBufferSize>()),
+        unixSocketPath(unixSocketPathIn), unixSocket(ios), dumpSize(0),
+        waitTimer(ios), connectRetryCount(0)
     {}
 
     /**
-     * @brief  Invokes InitiateOffload method of dump manager which
-     *         directs pldm to start writing on the nbd device.
+     * @brief Connects to unix socket to read dump data
      *
      * @return void
      */
-    void initiateOffloadOnNbdDevice()
+    void doConnect()
+    {
+        this->unixSocket.async_connect(
+            unixSocketPath.c_str(),
+            [this, self(shared_from_this())](boost::system::error_code ec) {
+                if (ec)
+                {
+                    // TODO:
+                    // right now we don't have dbus method which can make sure
+                    // unix socket is ready to accept connection so its possible
+                    // that bmcweb can try to connect to socket before even
+                    // socket setup, so using retry mechanism with timeout.
+                    if (ec == boost::system::errc::no_such_file_or_directory ||
+                        ec == boost::system::errc::connection_refused)
+                    {
+                        BMCWEB_LOG_DEBUG << "UNIX Socket: async_connect "
+                                         << ec.message() << ec;
+                        retrySocketConnect();
+                        return;
+                    }
+                    BMCWEB_LOG_ERROR << "UNIX Socket: async_connect error "
+                                     << ec.message() << ec;
+                    waitTimer.cancel();
+                    this->connection->sendStreamErrorStatus(
+                        boost::beast::http::status::internal_server_error);
+                    this->connection->close();
+                    return;
+                }
+                waitTimer.cancel();
+                this->connection->sendStreamHeaders(
+                    std::to_string(this->dumpSize), "application/octet-stream");
+                this->doReadStream();
+            });
+    }
+
+    /**
+     * @brief  Invokes InitiateOffload method of dump manager which
+     *         directs dump manager to start writing on unix domain socket.
+     *
+     * @return void
+     */
+    void initiateOffload()
     {
         crow::connections::systemBus->async_method_call(
             [this,
@@ -60,243 +99,212 @@ class Handler : public std::enable_shared_from_this<Handler>
                 if (ec)
                 {
                     BMCWEB_LOG_ERROR << "DBUS response error: " << ec;
-                    resetBuffers();
-                    resetHandler();
+                    this->connection->sendStreamErrorStatus(
+                        boost::beast::http::status::internal_server_error);
+                    this->connection->close();
                     return;
                 }
             },
             "xyz.openbmc_project.Dump.Manager",
             "/xyz/openbmc_project/dump/" + dumpType + "/entry/" + entryID,
-            "xyz.openbmc_project.Dump.Entry", "InitiateOffload", "/dev/nbd1");
+            "xyz.openbmc_project.Dump.Entry", "InitiateOffload",
+            unixSocketPath.c_str());
     }
 
     /**
-     * @brief  Kills nbd-proxy
+     * @brief  This function setup a timer for retrying unix socket connect.
      *
      * @return void
      */
-    void doClose()
+    void retrySocketConnect()
     {
-        int rc = kill(proxy.id(), SIGTERM);
-        if (rc)
-        {
-            return;
-        }
-        proxy.wait();
+        waitTimer.expires_after(std::chrono::milliseconds(500));
+
+        waitTimer.async_wait([this, self(shared_from_this())](
+                                 const boost::system::error_code& ec) {
+            if (ec)
+            {
+                BMCWEB_LOG_ERROR << "Async_wait failed " << ec;
+                return;
+            }
+
+            if (connectRetryCount < maxConnectRetryCount)
+            {
+                BMCWEB_LOG_DEBUG
+                    << "Calling doConnect() by checking retry count: "
+                    << connectRetryCount;
+                connectRetryCount++;
+                doConnect();
+            }
+            else
+            {
+                BMCWEB_LOG_ERROR
+                    << "Failed to connect, reached max retry count: "
+                    << connectRetryCount;
+                waitTimer.cancel();
+                this->connection->sendStreamErrorStatus(
+                    boost::beast::http::status::internal_server_error);
+                this->connection->close();
+                return;
+            }
+        });
     }
 
-    /**
-     * @brief  Starts nbd-proxy
-     *
-     * @return void
-     */
-    void connect()
+    void getDumpSize(const std::string& entryID, const std::string& dumpType)
     {
-        std::error_code ec;
-        proxy = boost::process::child("/usr/sbin/nbd-proxy", media,
-                                      boost::process::std_out > pipeOut,
-                                      boost::process::std_in < pipeIn, ec);
-        if (ec)
-        {
-            BMCWEB_LOG_ERROR << "Couldn't connect to nbd-proxy: "
-                             << ec.message();
-            resetHandler();
-            return;
-        }
-        doRead();
-    }
-
-    /**
-     * @brief  Wait for data on tcp socket from nbd-server.
-     *
-     * @return void
-     */
-    void waitForMessageOnSocket()
-    {
-
-        std::size_t bytes = inputBuffer->capacity() - inputBuffer->size();
-
-        (*stream).async_read_some(
-            inputBuffer->prepare(bytes),
+        crow::connections::systemBus->async_method_call(
             [this,
-             self(shared_from_this())](const boost::system::error_code& ec,
-                                       std::size_t bytes_transferred) {
+             self(shared_from_this())](const boost::system::error_code ec,
+                                       const std::variant<uint64_t>& size) {
                 if (ec)
                 {
-                    BMCWEB_LOG_DEBUG << "Error while reading on socket";
-                    doClose();
-                    resetBuffers();
-                    resetHandler();
+                    BMCWEB_LOG_ERROR
+                        << "DBUS response error: Unable to get the dump size "
+                        << ec;
+                    this->connection->sendStreamErrorStatus(
+                        boost::beast::http::status::internal_server_error);
+                    this->connection->close();
                     return;
                 }
-
-                inputBuffer->commit(bytes_transferred);
-                doWrite();
-            });
+                const uint64_t* dumpsize = std::get_if<uint64_t>(&size);
+                this->dumpSize = *dumpsize;
+                this->initiateOffload();
+                this->doConnect();
+            },
+            "xyz.openbmc_project.Dump.Manager",
+            "/xyz/openbmc_project/dump/" + dumpType + "/entry/" + entryID,
+            "org.freedesktop.DBus.Properties", "Get",
+            "xyz.openbmc_project.Dump.Entry", "Size");
     }
 
     /**
-     * @brief  Writes data on input pipe of nbd-client.
+     * @brief  Reads data from unix domain socket and writes on
+     *         http stream connection socket.
      *
      * @return void
      */
-    void doWrite()
+
+    void doReadStream()
     {
+        std::size_t bytes = outputBuffer.capacity() - outputBuffer.size();
 
-        if (doingWrite)
-        {
-            BMCWEB_LOG_DEBUG << "Already writing.  Bailing out";
-            return;
-        }
-
-        if (inputBuffer->size() == 0)
-        {
-            BMCWEB_LOG_DEBUG << "inputBuffer empty.  Bailing out";
-            return;
-        }
-
-        doingWrite = true;
-        boost::asio::async_write(
-            pipeIn, inputBuffer->data(),
-            [this, self(shared_from_this())](const boost::beast::error_code& ec,
-                                             std::size_t bytesWritten) {
-                if (ec)
-                {
-                    BMCWEB_LOG_DEBUG << "VM socket port closed";
-                    doClose();
-                    resetBuffers();
-                    resetHandler();
-                    return;
-                }
-
-                doingWrite = false;
-
-                if (negotiationDone == false)
-                {
-                    // "gDf" is NBD reply magic
-                    std::string reply_magic("gDf");
-                    std::string reply_string(
-                        static_cast<char*>(inputBuffer->data().data()),
-                        bytesWritten);
-                    std::size_t found = reply_string.find(reply_magic);
-                    if (found != std::string::npos)
-                    {
-                        negotiationDone = true;
-                        writeonnbd = true;
-                    }
-                }
-
-                inputBuffer->consume(bytesWritten);
-                waitForMessageOnSocket();
-                if (writeonnbd)
-                {
-                    // NBD Negotiation Complete!!!!. Notify Dump manager to
-                    // start dumping the actual data over NBD device
-                    initiateOffloadOnNbdDevice();
-                    writeonnbd = false;
-                }
-            });
-    }
-
-    /**
-     * @brief  Reads data on output pipe of nbd-client and write on
-     *         tcp socket.
-     *
-     * @return void
-     */
-    void doRead()
-    {
-        std::size_t bytes = outputBuffer->capacity() - outputBuffer->size();
-
-        pipeOut.async_read_some(
-            outputBuffer->prepare(bytes),
+        this->unixSocket.async_read_some(
+            outputBuffer.prepare(bytes),
             [this, self(shared_from_this())](
                 const boost::system::error_code& ec, std::size_t bytesRead) {
                 if (ec)
                 {
-                    BMCWEB_LOG_ERROR << "Couldn't read from VM port: " << ec;
-                    doClose();
-                    resetBuffers();
-                    resetHandler();
+                    BMCWEB_LOG_ERROR << "Couldn't read from local peer: " << ec;
+
+                    if (ec != boost::asio::error::eof)
+                    {
+                        BMCWEB_LOG_ERROR << "Couldn't read from local peer: "
+                                         << ec;
+                        this->connection->sendStreamErrorStatus(
+                            boost::beast::http::status::internal_server_error);
+                    }
+                    this->connection->close();
                     return;
                 }
 
-                outputBuffer->commit(bytesRead);
-
-                boost::asio::async_write(
-                    *stream, outputBuffer->data(),
-                    [this](const boost::system::error_code& ec2,
-                           std::size_t bytes_transferred) {
-                        if (ec2)
-                        {
-                            BMCWEB_LOG_DEBUG << "Error while writing on socket";
-                            doClose();
-                            resetBuffers();
-                            resetHandler();
-                            return;
-                        }
-
-                        outputBuffer->consume(bytes_transferred);
-                        doRead();
-                    });
+                outputBuffer.commit(bytesRead);
+                auto streamHandler = [this, bytesRead,
+                                      self(shared_from_this())]() {
+                    this->outputBuffer.consume(bytesRead);
+                    this->doReadStream();
+                };
+                this->connection->sendMessage(outputBuffer.data(),
+                                              streamHandler);
             });
     }
 
-    /**
-     * @brief  Resets input and output buffers.
-     * @return void
-     */
-    void resetBuffers()
-    {
-        this->inputBuffer->clear();
-        this->outputBuffer->clear();
-    }
-
-    boost::process::async_pipe pipeOut;
-    boost::process::async_pipe pipeIn;
-    boost::process::child proxy;
-    std::string media;
     std::string entryID;
     std::string dumpType;
-    bool doingWrite;
-    bool negotiationDone;
-    bool writeonnbd;
-    std::unique_ptr<boost::beast::flat_static_buffer<nbdBufferSize>>
-        outputBuffer;
-    std::unique_ptr<boost::beast::flat_static_buffer<nbdBufferSize>>
-        inputBuffer;
-    std::shared_ptr<crow::Request::Adaptor> stream;
+    boost::beast::flat_static_buffer<socketBufferSize> outputBuffer;
+    std::filesystem::path unixSocketPath;
+    stream_protocol::socket unixSocket;
+    uint64_t dumpSize;
+    boost::asio::steady_timer waitTimer;
+    crow::streaming_response::Connection* connection = nullptr;
+    uint16_t connectRetryCount;
 };
 
-static std::shared_ptr<Handler> handler;
-inline void resetHandler()
+static boost::container::flat_map<crow::streaming_response::Connection*,
+                                  std::shared_ptr<Handler>>
+    handlers;
+
+inline void requestRoutes(App& app)
 {
+    BMCWEB_ROUTE(app,
+                 "/redfish/v1/Managers/bmc/LogServices/Dump/attachment/<str>")
+        .privileges({"ConfigureComponents", "ConfigureManager"})
+        .streamingResponse()
+        .onopen([](crow::streaming_response::Connection& conn) {
+            std::string url(conn.req.target());
+            std::size_t pos = url.rfind('/');
+            std::string dumpId;
+            if (pos != std::string::npos)
+            {
+                dumpId = url.substr(pos + 1);
+            }
 
-    handler.reset();
+            std::string dumpType = "bmc";
+            boost::asio::io_context* ioCon = conn.getIoContext();
+
+            std::string unixSocketPath =
+                unixSocketPathDir + dumpType + "_dump_" + dumpId;
+
+            handlers[&conn] = std::make_shared<Handler>(
+                *ioCon, dumpId, dumpType, unixSocketPath);
+            handlers[&conn]->connection = &conn;
+            handlers[&conn]->getDumpSize(dumpId, dumpType);
+        })
+        .onclose([](crow::streaming_response::Connection& conn) {
+            auto handler = handlers.find(&conn);
+            if (handler == handlers.end())
+            {
+                BMCWEB_LOG_DEBUG << "No handler to cleanup";
+                return;
+            }
+            handler->second->outputBuffer.clear();
+            handlers.erase(handler);
+        });
+
+    BMCWEB_ROUTE(app,
+                 "/redfish/v1/Systems/system/LogServices/Dump/attachment/<str>")
+        .privileges({"ConfigureComponents", "ConfigureManager"})
+        .streamingResponse()
+        .onopen([](crow::streaming_response::Connection& conn) {
+            std::string url(conn.req.target());
+            std::size_t pos = url.rfind('/');
+            std::string dumpId;
+            if (pos != std::string::npos)
+            {
+                dumpId = url.substr(pos + 1);
+            }
+
+            std::string dumpType = "system";
+            boost::asio::io_context* ioCon = conn.getIoContext();
+
+            std::string unixSocketPath =
+                unixSocketPathDir + dumpType + "_dump_" + dumpId;
+
+            handlers[&conn] = std::make_shared<Handler>(
+                *ioCon, dumpId, dumpType, unixSocketPath);
+            handlers[&conn]->connection = &conn;
+            handlers[&conn]->getDumpSize(dumpId, dumpType);
+        })
+        .onclose([](crow::streaming_response::Connection& conn) {
+            auto handler = handlers.find(&conn);
+            if (handler == handlers.end())
+            {
+                BMCWEB_LOG_DEBUG << "No handler to cleanup";
+                return;
+            }
+            handlers.erase(handler);
+            handler->second->outputBuffer.clear();
+        });
 }
-inline void handleDumpOffloadUrl(const crow::Request& req, crow::Response& res,
-                                 const std::string& entryId,
-                                 const std::string& dumpEntryType)
-{
 
-    // Run only one instance of Handler, one dump offload can happen at a time
-    if (handler != nullptr)
-    {
-        BMCWEB_LOG_ERROR << "Handler already running";
-        res.result(boost::beast::http::status::service_unavailable);
-        res.jsonValue["Description"] = "Service is already being used";
-        res.end();
-        return;
-    }
-
-    const char* media = "1";
-    boost::asio::io_context* io_con = req.ioService;
-
-    handler = std::make_shared<Handler>(media, *io_con, entryId, dumpEntryType);
-    handler->stream =
-        std::make_shared<crow::Request::Adaptor>(std::move(req.socket()));
-    handler->connect();
-    handler->waitForMessageOnSocket();
-}
 } // namespace obmc_dump
 } // namespace crow
