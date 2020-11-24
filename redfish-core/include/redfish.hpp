1afe3f87f66ecaa87f8f431d92869aac605f8866
George Liu <liuxiwei@inspur.com>
2020-11-24 16:47:50 +0800

    Implement SubProcessors for processor core
    - Add the SubProcessors class to bmcweb to support processor cores.
    
    - The SubProcessors collection is a collection under the processor
      schema. An association, <insert association name>, is used to link
      the core to the processor.
    
    Tested: Validator passes
    curl -k -H "X-Auth-Token: $token"
    https://${bmc}/redfish/v1/Systems/system/Processors/cpu0
    {
      "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0",
      "@odata.type": "#Processor.v1_11_0.Processor",
      "Id": "cpu0",
      "InstructionSet": "PowerISA",
      "Manufacturer": "IBM",
      "SubProcessors": {
        "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors"
      }
        ... ...
    }
    
    curl -k -H "X-Auth-Token: $token"
    https://${bmc}/redfish/v1/Systems/system/Processors/cpu0/SubProcessors
    {
      "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors",
      "@odata.type": "#ProcessorCollection.ProcessorCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors/core0"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors/core1"
        }
        ... ...
      ],
      ... ...
    }
    
    curl -k -H "X-Auth-Token: $token"
    https://${bmc}/redfish/v1/Systems/system/Processors/cpu0/SubProcessors/core1
    {
      "@odata.id": "/redfish/v1/Systems/system/Processors/cpu0/SubProcessors/core1",
      "@odata.type": "#Processor.v1_11_0.Processor",
      "Id": "core1",
        "Name": "",
        "Status": {
            "Health": "OK",
            "State": "Enabled"
        }
    }
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: If155b97b0c782d82541c00ecf5ee70cb0180f71f
    
    Conflicts:
    	redfish-core/lib/processor.hpp

--- redfish-core/include/redfish.hpp
+++ redfish-core/include/redfish.hpp
@@ -185,6 +185,8 @@ class RedfishService
         requestRoutesOperatingConfig(app);
         requestRoutesMemoryCollection(app);
         requestRoutesMemory(app);
+        requestRoutesSubProcessors(app);
+        requestRoutesSubProcessorsCore(app);
 
         requestRoutesSystemsCollection(app);
         requestRoutesSystems(app);
