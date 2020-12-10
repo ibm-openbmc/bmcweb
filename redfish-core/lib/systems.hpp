7903cbf24b4878e85591e1ba838fd9ee2ad94327
Chicago Duan <duanzhijia01@inspur.com>
2020-12-10 19:50:16 +0800

    Redfish: Implement OEM lamp test
    Implement OEM lamp test in redfish/v1/Systems/system
    
    IBM oem Lamptest is a switch used to test all leds.When the "lamptest"
    status is set to true, all leds are lit. When "lamptest" is set to
    false or timeout(default is 4 minutes and can be configured), the
    lamptest is finished and all leds revert to their original state.
    
    The existing LocationIndicator interfaces is not suitable to this
    task, because we have to light all the leds, not just one identity
    led.
    
    We need set the asserted property of
    /xyz/openbmc_project/led/groups/lamp_test to true as part of PATCH
    if the request is true. Similarly, it could also set asserted to
    false if the request is to false.
    
    We use macros to control the URL, which need to be enabled in
    bmcweb_%.bbappend to see it, for example:
    EXTRA_OEMESON_append = " \
        -Dibm-lamp-test=enabled \
        "
    
    I used a test version of dbus
    object(xyz.openbmc_project.LED.GroupManager) to test this URL.
    When I set the Redfish LampTest to true or false, the output
    from the "busctl get-property ..." is changed.
    for example :
    busctl get-property xyz.openbmc_project.LED.GroupManager
                       /xyz/openbmc_project/led/groups/lamp_test
                       xyz.openbmc_project.Led.Group Asserted
    b true
    
    Tested:
    Validator no error
    1、Start the lamp test:
       curl -k -H "X-Auth-Token: $token" -X PATCH
       https://${bmc}/redfish/v1/Systems/system -d
       '{"Oem":{"IBM":{"LampTest": true}}}'
    
    When we set Redfish LampTest to true, the output of
    busctl get-property on /xyz/openbmc_project/led/groups/lamp_test
    is also true:
    
    busctl get-property xyz.openbmc_project.LED.GroupManager
                       /xyz/openbmc_project/led/groups/lamp_test
                       xyz.openbmc_project.Led.Group Asserted
    b true
    
    2、Stop the lamp test:
       curl -k -H "X-Auth-Token: $token" -X PATCH
       https://${bmc}/redfish/v1/Systems/system -d
       '{"Oem":{"IBM":{"LampTest": false}}}'
    
    3、 Get the lamp test status:
       We use set-propery to set the Asserted to false:
       busctl set-property xyz.openbmc_project.LED.GroupManager
                       /xyz/openbmc_project/led/groups/lamp_test
                       xyz.openbmc_project.Led.Group Asserted b false
    
       Then we will see that Redfish LampTest is false:
        curl -k -H "X-Auth-Token: $token" -XGET
        https://${bmc}/redfish/v1/Systems/system
        {
          "@odata.id": "/redfish/v1/Systems/system",
          "@odata.type": "#ComputerSystem.v1_13_0.ComputerSystem",
          "Actions": {
             "#ComputerSystem.Reset": {
             "@Redfish.ActionInfo":
             	 "/redfish/v1/Systems/system/ResetActionInfo",
             "target":
              "/redfish/v1/Systems/system/Actions/ComputerSystem.Reset"
          }
        },
        "Bios": {
          "@odata.id": "/redfish/v1/Systems/system/Bios"
        },
    
        ... ...
    
       "Oem": {
         "@odata.type": "#OemComputerSystem.Oem",
         "IBM": {
           "@odata.type": "#OemComputerSystem.IBM",
           "LampTest": false
         }
       },
    
        ... ...
    
        "SystemType": "Physical"
        }
    
    Signed-off-by: Chicago Duan <duanzhijia01@inspur.com>
    Change-Id: Icffdb01581c37957bdc95dcd36cd83a126fd5859

--- redfish-core/lib/systems.hpp
+++ redfish-core/lib/systems.hpp
@@ -19,7 +19,9 @@
 #include "led.hpp"
 #include "pcie.hpp"
 #include "redfish_util.hpp"
-
+#ifdef BMCWEB_ENABLE_IBM_LAMP_TEST
+#include "oem/ibm/lamp_test.hpp"
+#endif
 #include <app.hpp>
 #include <boost/container/flat_map.hpp>
 #include <registries/privilege_registry.hpp>
@@ -2831,6 +2833,9 @@ inline void requestRoutesSystems(App& app)
             getPowerRestorePolicy(asyncResp);
             getAutomaticRetry(asyncResp);
             getLastResetTime(asyncResp);
+#ifdef BMCWEB_ENABLE_IBM_LAMP_TEST
+            getLampTestState(asyncResp);
+#endif
 #ifdef BMCWEB_ENABLE_REDFISH_PROVISIONING_FEATURE
             getProvisioningStatus(asyncResp);
 #endif
@@ -2850,12 +2855,14 @@ inline void requestRoutesSystems(App& app)
                 std::optional<std::string> powerRestorePolicy;
                 std::optional<std::string> powerMode;
                 std::optional<bool> trustedModuleRequiredToBoot;
+                std::optional<nlohmann::json> oem;
+
                 if (!json_util::readJson(
                         req, asyncResp->res, "IndicatorLED", indicatorLed,
                         "LocationIndicatorActive", locationIndicatorActive,
                         "Boot", bootProps, "WatchdogTimer", wdtTimerProps,
                         "PowerRestorePolicy", powerRestorePolicy, "AssetTag",
-                        assetTag, "PowerMode", powerMode,
+                        assetTag, "PowerMode", powerMode, "Oem", oem,
                         "TrustedModuleRequiredToBoot",
                         trustedModuleRequiredToBoot))
                 {
@@ -2933,6 +2940,7 @@ inline void requestRoutesSystems(App& app)
                     setPowerRestorePolicy(asyncResp, *powerRestorePolicy);
                 }
 
+
                 if (powerMode)
                 {
                     setPowerMode(asyncResp, *powerMode);
@@ -2942,6 +2950,33 @@ inline void requestRoutesSystems(App& app)
                 {
                     setTrustedModuleRequiredToBoot(
                         asyncResp, *trustedModuleRequiredToBoot);
+                }
+
+                if (oem)
+                {
+                    std::optional<nlohmann::json> ibmOem;
+                    if (!redfish::json_util::readJson(*oem, asyncResp->res,
+                                                      "IBM", ibmOem))
+                    {
+                        return;
+                    }
+
+                    if (ibmOem)
+                    {
+                        std::optional<bool> lampTest;
+                        if (!json_util::readJson(*ibmOem, asyncResp->res,
+                                                 "LampTest", lampTest))
+                        {
+                            return;
+                        }
+
+                        if (lampTest)
+                        {
+#ifdef BMCWEB_ENABLE_IBM_LAMP_TEST
+                            setLampTestState(asyncResp, *lampTest);
+#endif
+                        }
+                    }
                 }
             });
 }
