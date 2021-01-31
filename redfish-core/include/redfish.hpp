a9266fff2e4e7b7f17a03306af8bab0a1268644b
SunnySrivastava1984 <sunnsr25@in.ibm.com>
2021-01-31 12:30:52 -0600

    Implementation of Port schema in bmcweb
    This commit implemets Port and Port collection schema
    on bmcweb.
    The schema describes a port of a switch, controller, chassis,
    or any other device that could be connected to another entity.
    
    Current use case:
    It is implemented to model collection of CXP ports on fabric
    adapters.
    
    Validator has been executed and no new error has been found.
    
    Sample output:
    curl -k -H "X-Auth-Token: $bmc_token" -X GET https://${bmc}/redfish/v1/Systems/system/FabricAdapters/pcie_cable_card9
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/pcie_cable_card9",
      "@odata.type": "#FabricAdapter.v1_0_0.FabricAdapter",
      "Id": "pcie_cable_card9",
      "Name": "",
      "Ports": {
        "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/pcie_cable_card9/Ports"
      }
    }
    
    curl -k -H "X-Auth-Token: $bmc_token" -X GET https://${bmc}/redfish/v1/Systems/system/FabricAdapters/pcie_cable_card9/Ports
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/pcie_cable_card9/Ports",
      "@odata.type": "#PortCollection.PortCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/pcie_cable_card9/Ports/cxp_bot"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/pcie_cable_card9/Ports/cxp_top"
        }
      ],
      "Members@odata.count": 2,
      "Name": "Port Collection"
    }
    
    curl -k -H "X-Auth-Token: $bmc_token" -X GET https://${bmc}/redfish/v1/Systems/system/FabricAdapters/pcie_cable_card9/Ports/cxp_bot
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/pcie_cable_card9/Ports/cxp_bot",
      "@odata.type": "#Port.v1_3_0.Port",
      "Id": "Port",
      "Location": {
        "PartLocation": {
          "ServiceLabel": "U78DA.ND0.1234567-P0-C9-T1"
        }
      },
      "Name": ""
    }
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Change-Id: Iaeb728d2326ca49f1f00a459d1f4e1021146a83c

--- redfish-core/include/redfish.hpp
+++ redfish-core/include/redfish.hpp
@@ -30,6 +30,7 @@
 #include "../lib/network_protocol.hpp"
 #include "../lib/pcie.hpp"
 #include "../lib/pcie_slots.hpp"
+#include "../lib/port.hpp"
 #include "../lib/power.hpp"
 #include "../lib/processor.hpp"
 #include "../lib/redfish_sessions.hpp"
@@ -96,6 +97,8 @@ class RedfishService
         nodes.emplace_back(std::make_unique<FabricAdapters>(app));
         nodes.emplace_back(std::make_unique<FabricAdapterCollection>(app));
         nodes.emplace_back(std::make_unique<PCIeSlots>(app));
+        nodes.emplace_back(std::make_unique<Port>(app));
+        nodes.emplace_back(std::make_unique<PortCollection>(app));
 #ifdef BMCWEB_INSECURE_ENABLE_REDFISH_FW_TFTP_UPDATE
         nodes.emplace_back(
             std::make_unique<UpdateServiceActionsSimpleUpdate>(app));
