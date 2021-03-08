4cdd59f83c2ecb5432ca7a981657a2dc35ce6cbf
zhanghch05 <zhanghch05@inspur.com>
2021-03-08 19:04:35 +0800

    Add Redfish ThermalMetrics schema in bmcweb
    The ThermalMetrics is a new resource in Redfish version 2020.4.
    It contains an array of temperature readings.
    It is a child of the ThermalSubsystem schema.
    The current value of each temperature sensor and the corresponding
    link are enumerated in this collection.
    
    Reference:
    https://www.dmtf.org/sites/default/files/standards/documents/DSP0268_2020.4.pdf
    https://redfish.dmtf.org/schemas/v1/ThermalMetrics.v1_0_0.json
    
    Test:
    1. Validator passed.
    2. doGet method:
    ~$  curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Chassis/chassis/ThermalSubsystem/ThermalMetrics
    {
      "@odata.id": "/redfish/v1/Chassis/chassis/ThermalSubsystem/ThermalMetrics",
      "@odata.type": "#ThermalMetrics.v1_0_0.ThermalMetrics",
      "Id": "ThermalMetrics",
      "Name": "Chassis Thermal Metrics",
      "TemperatureReadingsCelsius": [
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Sensors/psu_inlet",
          "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/psu_inlet",
          "DeviceName": "psu_inlet",
          "Reading": 28.25
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Sensors/bmc_zone",
          "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/bmc_zone",
          "DeviceName": "bmc_zone",
          "Reading": 30.375
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Sensors/outlet",
          "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/outlet",
          "DeviceName": "outlet",
          "Reading": 31.75
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Sensors/ocp_zone",
          "DataSourceUri": "/redfish/v1/Chassis/chassis/Sensors/ocp_zone",
          "DeviceName": "ocp_zone",
          "Reading": 33.937
        }
      ]
    }
    
    3. A bad chassis ID:
    ~$  curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Chassis/chassisBAD/ThermalSubsystem/ThermalMetrics
    {
      "error": {
        "@Message.ExtendedInfo": [
          {
            "@odata.type": "#Message.v1_1_1.Message",
            "Message": "The requested resource of type ThermalMetrics named chassisBAD was not found.",
            "MessageArgs": [
              "ThermalMetrics",
              "chassisBAD"
            ],
            "MessageId": "Base.1.8.1.ResourceNotFound",
            "MessageSeverity": "Critical",
            "Resolution": "Provide a valid resource identifier and resubmit the request."
          }
        ],
        "code": "Base.1.8.1.ResourceNotFound",
        "message": "The requested resource of type ThermalMetrics named chassisBAD was not found."
      }
    }
    
    Signed-off-by: zhanghaicheng <zhanghch05@inspur.com>
    Change-Id: Ib4182e7dc6e204371636a33a391e8e2a58dad113

--- redfish-core/include/redfish.hpp
+++ redfish-core/include/redfish.hpp
@@ -45,6 +45,7 @@
 #include "../lib/task.hpp"
 #include "../lib/telemetry_service.hpp"
 #include "../lib/thermal.hpp"
+#include "../lib/thermal_metrics.hpp"
 #include "../lib/thermal_subsystem.hpp"
 #include "../lib/update_service.hpp"
 #include "../lib/virtual_media.hpp"
@@ -78,6 +79,7 @@ class RedfishService
         requestRoutesPower(app);
 #endif
         requestRoutesThermalSubsystem(app);
+        requestRoutesThermalMetrics(app);
         requestRoutesManagerCollection(app);
         requestRoutesManager(app);
         requestRoutesManagerResetAction(app);
