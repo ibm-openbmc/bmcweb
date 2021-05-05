77e042559e09f8a46edf7d81bca7d13956e02fa9
Asmitha Karunanithi <asmitk01@in.ibm.com>
2021-05-05 12:10:11 -0500

    Add support for hostboot dump
    This commit contains changes that supports listing and
    deletion of hostboot dump. The hostboot dumps will be listed under
    /redfish/v1/Systems/system/LogServices/Dump along with
    the system & resource dump entries with the entry id prepended with
    "Hostboot_".
    
    Tested-By:
    
    curl -k -H "X-Auth-Token:$bmc_token" -X GET https://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Entries
    
    curl -k -H "X-Auth-Token:$bmc_token" -X GET https://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Entries/Hostboot_6
    {
      "@odata.id": "/redfish/v1/Systems/system/LogServices/Dump/Entries/Hostboot_6",
      "@odata.type": "#LogEntry.v1_7_0.LogEntry",
      "AdditionalDataSizeBytes": 0,
      "AdditionalDataURI": "/redfish/v1/Systems/system/LogServices/Dump/attachment/Hostboot_6",
      "Created": "1970-01-01T00:00:00+00:00",
      "DiagnosticDataType": "OEM",
      "EntryType": "Event",
      "Id": "Hostboot_6",
      "Name": "System Dump Entry",
      "OEMDiagnosticDataType": "Hostboot"
    }
    
    curl -k -H "X-Auth-Token:$bmc_token" -X DELETE https://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Entries/Hostboot_6
    
    curl -k -H "X-Auth-Token:$bmc_token" -X POST https://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Actions/LogService.ClearLog
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>

--- redfish-core/lib/log_services.hpp
+++ redfish-core/lib/log_services.hpp
@@ -530,7 +530,7 @@ inline void
                         "/redfish/v1/Systems/system/LogServices/Dump/"
                         "attachment/" +
                         dumpEntryId;
-                    thisEntry["Name"] = "System Dump Entry";
+                    thisEntry["Name"] = dumpType + " Dump Entry";
                     thisEntry["OEMDiagnosticDataType"] = dumpType;
                 }
             }
@@ -924,9 +924,9 @@ inline void clearDump(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                       const std::string& dumpType)
 {
     std::string dumpInterface;
-    if (dumpType == "Hostboot")
+    if (dumpType == "Resource" || dumpType == "Hostboot")
     {
-        dumpInterface = "xyz.openbmc_project.Dump.Entry";
+        dumpInterface = "com.ibm.Dump.Entry." + dumpType;
     }
     else
     {
