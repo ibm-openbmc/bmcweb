86d8d9a5374fa275c1b2071960850b027e1270ba
Asmitha Karunanithi <asmitk01@in.ibm.com>
2021-05-12 01:46:10 -0500

    Add support for hostboot dump
    This commit contains changes that supports listing and
    deletion of hostboot dump. The hostboot dumps will be listed under
    /redfish/v1/Systems/system/LogServices/Dump along with
    the system & resource dump entries with the entry id prepended with
    "Hostboot_".
    
    Tested-By:
    
    curl -k -H "X-Auth-Token:$bmc_token" -X GET https://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Entries
    
    curl -k -H "X-Auth-Token:$bmc_token" -X GET https://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Entries/Hostboot_6
    {
      "@odata.id": "/redfish/v1/Systems/system/LogServices/Dump/Entries/Hostboot_6",
      "@odata.type": "#LogEntry.v1_7_0.LogEntry",
      "AdditionalDataSizeBytes": 0,
      "AdditionalDataURI": "/redfish/v1/Systems/system/LogServices/Dump/attachment/Hostboot_6",
      "Created": "1970-01-01T00:00:00+00:00",
      "DiagnosticDataType": "OEM",
      "EntryType": "Event",
      "Id": "Hostboot_6",
      "Name": "System Dump Entry",
      "OEMDiagnosticDataType": "Hostboot"
    }
    
    curl -k -H "X-Auth-Token:$bmc_token" -X DELETE https://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Entries/Hostboot_6
    
    curl -k -H "X-Auth-Token:$bmc_token" -X POST https://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Actions/LogService.ClearLog
    
    Redfish Validator Passed!
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: I013160930cc2a5f9939501cff80403ce3c35c837

--- redfish-core/lib/log_services.hpp
+++ redfish-core/lib/log_services.hpp
@@ -411,7 +411,8 @@ inline void
     {
         dumpPath = "/redfish/v1/Managers/bmc/LogServices/Dump/Entries/";
     }
-    else if (dumpType == "System" || dumpType == "Resource")
+    else if (dumpType == "System" || dumpType == "Resource" ||
+             dumpType == "Hostboot")
     {
         dumpPath = "/redfish/v1/Systems/system/LogServices/Dump/Entries/";
     }
@@ -521,7 +522,8 @@ inline void
                         "/redfish/v1/Managers/bmc/LogServices/Dump/Entries/" +
                         entryID + "/attachment";
                 }
-                else if (dumpType == "System" || dumpType == "Resource")
+                else if (dumpType == "System" || dumpType == "Resource" ||
+                         dumpType == "Hostboot")
                 {
                     std::string dumpEntryId(dumpType + "_");
                     dumpEntryId.append(entryID);
@@ -553,7 +555,8 @@ inline void
         dumpPath = "/redfish/v1/Managers/bmc/LogServices/Dump/Entries/";
         dumpId = entryID;
     }
-    else if (dumpType == "System" || dumpType == "Resource")
+    else if (dumpType == "System" || dumpType == "Resource" ||
+             dumpType == "Hostboot")
     {
         dumpPath = "/redfish/v1/Systems/system/LogServices/Dump/Entries/";
         std::size_t pos = entryID.find_first_of('_');
@@ -658,7 +661,8 @@ inline void
                         "/redfish/v1/Managers/bmc/LogServices/Dump/Entries/" +
                         entryID + "/attachment";
                 }
-                else if (dumpType == "System" || dumpType == "Resource")
+                else if (dumpType == "System" || dumpType == "Resource" ||
+                         dumpType == "Hostboot")
                 {
                     std::string dumpAttachment(
                         "/redfish/v1/Systems/system/LogServices/Dump/Entries/");
@@ -766,6 +770,12 @@ inline void createDumpTaskCallback(
     {
         dumpEntryPath =
             "/redfish/v1/Systems/system/LogServices/Dump/Entries/Resource_" +
+            dumpId;
+    }
+    else if (dumpPath == "/xyz/openbmc_project/dump/hostboot/entry")
+    {
+        dumpEntryPath =
+            "/redfish/v1/Systems/system/LogServices/Dump/Entries/Hostboot_" +
             dumpId;
     }
     else
@@ -942,7 +952,7 @@ inline void clearDump(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                       const std::string& dumpType)
 {
     std::string dumpInterface;
-    if (dumpType == "Resource")
+    if (dumpType == "Resource" || dumpType == "Hostboot")
     {
         dumpInterface = "com.ibm.Dump.Entry." + dumpType;
     }
@@ -2371,10 +2381,11 @@ inline void requestRoutesSystemDumpEntryCollection(App& app)
                     "/redfish/v1/Systems/system/LogServices/Dump/Entries";
                 asyncResp->res.jsonValue["Name"] = "System Dump Entries";
                 asyncResp->res.jsonValue["Description"] =
-                    "Collection of System & Resource Dump Entries";
+                    "Collection of System, Resource & Hostboot Dump Entries";
 
                 getDumpEntryCollection(asyncResp, "System");
                 getDumpEntryCollection(asyncResp, "Resource");
+                getDumpEntryCollection(asyncResp, "Hostboot");
             });
 }
 
@@ -2395,6 +2406,10 @@ inline void requestRoutesSystemDumpEntry(App& app)
                 else if (boost::starts_with(param, "Resource"))
                 {
                     getDumpEntryById(asyncResp, param, "Resource");
+                }
+                else if (boost::starts_with(param, "Hostboot"))
+                {
+                    getDumpEntryById(asyncResp, param, "Hostboot");
                 }
                 else
                 {
@@ -2426,6 +2441,10 @@ inline void requestRoutesSystemDumpEntry(App& app)
                 else if (boost::starts_with(param, "Resource"))
                 {
                     deleteDumpEntry(asyncResp, dumpId, "resource");
+                }
+                else if (boost::starts_with(param, "Hostboot"))
+                {
+                    deleteDumpEntry(asyncResp, dumpId, "hostboot");
                 }
                 else
                 {
@@ -2461,6 +2480,7 @@ inline void requestRoutesSystemDumpClear(App& app)
             {
                 clearDump(asyncResp, "System");
                 clearDump(asyncResp, "Resource");
+                clearDump(asyncResp, "Hostboot");
             });
 }
 
