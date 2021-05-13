69a57f17aae24e7c7873d804ffd29899de00f28b
Sunny Srivastava <sunnsr25@in.ibm.com>
2021-05-13 05:26:07 -0500

    Revert "Merge pull request #139 from sunnsr25/port"
    This reverts commit 0d76358b383453996dbdb5743676aa1f82101aec, reversing
    changes made to 38af613484994a3f7c12dd84e16e6fc1ebb85086.

--- redfish-core/include/redfish.hpp
+++ redfish-core/include/redfish.hpp
@@ -29,8 +29,6 @@
 #include "../lib/metric_report_definition.hpp"
 #include "../lib/network_protocol.hpp"
 #include "../lib/pcie.hpp"
-#include "../lib/pcie_slots.hpp"
-#include "../lib/port.hpp"
 #include "../lib/power.hpp"
 #include "../lib/processor.hpp"
 #include "../lib/redfish_sessions.hpp"
@@ -46,7 +44,6 @@
 #ifdef BMCWEB_ENABLE_VM_NBDPROXY
 #include "../lib/virtual_media.hpp"
 #endif // BMCWEB_ENABLE_VM_NBDPROXY
-#include "../lib/FabricAdapters.hpp"
 #include "../lib/assembly.hpp"
 #include "../lib/hypervisor_system.hpp"
 
@@ -94,11 +91,6 @@ class RedfishService
         nodes.emplace_back(std::make_unique<Storage>(app));
         nodes.emplace_back(std::make_unique<Drive>(app));
         nodes.emplace_back(std::make_unique<Assembly>(app));
-        nodes.emplace_back(std::make_unique<FabricAdapters>(app));
-        nodes.emplace_back(std::make_unique<FabricAdapterCollection>(app));
-        nodes.emplace_back(std::make_unique<PCIeSlots>(app));
-        nodes.emplace_back(std::make_unique<Port>(app));
-        nodes.emplace_back(std::make_unique<PortCollection>(app));
 #ifdef BMCWEB_INSECURE_ENABLE_REDFISH_FW_TFTP_UPDATE
         nodes.emplace_back(
             std::make_unique<UpdateServiceActionsSimpleUpdate>(app));
