d3c7a7e73d5b33bfc542a8246acb63fda451c349
Abhishek Patel <Abhishek.Patel@ibm.com>
2021-07-21 11:29:45 -0500

    Added SubordinateOverrides & Fix Log_services privileges
    SubordinateOverrides:
      This commit attempts to automate the creation of SubordinateOverrides
      privileges structures from the redfish privilege registry. In
      addition, it enhances the function of parse_registries.py.
    
      It read SubordinateOverrides privilege registry from DMTF and
      generates const defines SubordinateOverrides for all the privilege
      registry entries in the same format that the Privileges struct
      accepts.
    
      Moreover, it generates unique const defines for all
      SubordinateOverrides target levels.
      Ex: EthernetInterface SubordinateOverrides has two "Targets":
          ["Manager", "EthernetInterfaceCollection"]. So
          parse_registries.py generates two unique const
    
          1) Subordinate override for Manager -> EthernetInterface
          2) Subordinate override for Manager ->
             EthernetInterfaceCollection ->  EthernetInterface
    
      Note: if SubordinateOverrides privilege gets changed, then it
      automatically updates that route privilege, but if
      SubordinateOverrides target gets change, then the user needs to
      update that manually.
    
    Fix Log_services privileges:
      In Log_services, some of the privileges not following the
      Redfish_1.1.0_PrivilegeRegistry registry.
    
      privilege Change this commit contains
      Get method:
        1) /redfish/v1/Systems/system/LogServices/Crashdump/
            ConfigureManager -> Login
    
        2)/redfish/v1/Systems/system/LogServices/Crashdump/Entries/
             ConfigureManager -> Login
    
        3) /redfish/v1/Systems/system/LogServices/Crashdump/Entries/<str>/
            ConfigureComponents -> Login
    
        Impact: ConfigureManager -> Login    and
              ConfigureComponents -> Login
    
        This change allows Admin, Operator, and Readonly users to access
        Crashdump data and related entries.
    
    Tested: manually tested on Witherspoon system, there is no change in
            output. Run Redfish validator, with all different Privileges;
            Error Get: UUID: String '' does not match pattern ''
            this commit doesn't affect UUID
    
    Email sent to openbmc list:
    https://lists.ozlabs.org/pipermail/openbmc/2021-August/027232.html
    
    Signed-off-by: Abhishek Patel <Abhishek.Patel@ibm.com>
    Change-Id: I37d8a2882f1cfaa59a482083f180fdd0805e2e7d

--- redfish-core/lib/log_services.hpp
+++ redfish-core/lib/log_services.hpp
@@ -1222,7 +1222,8 @@ inline void requestRoutesJournalEventLogClear(App& app)
 {
     BMCWEB_ROUTE(app, "/redfish/v1/Systems/system/LogServices/EventLog/Actions/"
                       "LogService.ClearLog/")
-        .privileges({{"ConfigureComponents"}})
+        .privileges(redfish::privileges::
+                        postLogServiceSubOverComputerSystemLogServiceCollection)
         .methods(boost::beast::http::verb::post)(
             [](const crow::Request&,
                const std::shared_ptr<bmcweb::AsyncResp>& asyncResp) {
@@ -2617,9 +2618,7 @@ inline void requestRoutesCrashdumpService(App& app)
      * Functions triggers appropriate requests on DBus
      */
     BMCWEB_ROUTE(app, "/redfish/v1/Systems/system/LogServices/Crashdump/")
-        // This is incorrect, should be:
-        //.privileges(redfish::privileges::getLogService)
-        .privileges({{"ConfigureManager"}})
+        .privileges(redfish::privileges::getLogService)
         .methods(
             boost::beast::http::verb::
                 get)([](const crow::Request&,
@@ -2660,9 +2659,8 @@ void inline requestRoutesCrashdumpClear(App& app)
     BMCWEB_ROUTE(app,
                  "/redfish/v1/Systems/system/LogServices/Crashdump/Actions/"
                  "LogService.ClearLog/")
-        // This is incorrect, should be:
-        //.privileges(redfish::privileges::postLogService)
-        .privileges({{"ConfigureComponents"}})
+        .privileges(redfish::privileges::
+                        postLogServiceSubOverComputerSystemLogServiceCollection)
         .methods(boost::beast::http::verb::post)(
             [](const crow::Request&,
                const std::shared_ptr<bmcweb::AsyncResp>& asyncResp) {
@@ -2746,9 +2744,7 @@ inline void requestRoutesCrashdumpEntryCollection(App& app)
      */
     BMCWEB_ROUTE(app,
                  "/redfish/v1/Systems/system/LogServices/Crashdump/Entries/")
-        // This is incorrect, should be.
-        //.privileges(redfish::privileges::postLogEntryCollection)
-        .privileges({{"ConfigureComponents"}})
+        .privileges(redfish::privileges::getLogEntryCollection)
         .methods(
             boost::beast::http::verb::
                 get)([](const crow::Request&,
@@ -2822,9 +2818,7 @@ inline void requestRoutesCrashdumpEntry(App& app)
 
     BMCWEB_ROUTE(
         app, "/redfish/v1/Systems/system/LogServices/Crashdump/Entries/<str>/")
-        // this is incorrect, should be
-        // .privileges(redfish::privileges::getLogEntry)
-        .privileges({{"ConfigureComponents"}})
+        .privileges(redfish::privileges::getLogEntry)
         .methods(boost::beast::http::verb::get)(
             [](const crow::Request&,
                const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
@@ -2928,9 +2922,8 @@ inline void requestRoutesCrashdumpCollect(App& app)
     // method for security reasons.
     BMCWEB_ROUTE(app, "/redfish/v1/Systems/system/LogServices/Crashdump/"
                       "Actions/LogService.CollectDiagnosticData/")
-        // The below is incorrect;  Should be ConfigureManager
-        //.privileges(redfish::privileges::postLogService)
-        .privileges({{"ConfigureComponents"}})
+        .privileges(redfish::privileges::
+                        postLogServiceSubOverComputerSystemLogServiceCollection)
         .methods(
             boost::beast::http::verb::
                 post)([](const crow::Request& req,
@@ -3115,9 +3108,8 @@ inline void requestRoutesPostCodesClear(App& app)
     BMCWEB_ROUTE(app,
                  "/redfish/v1/Systems/system/LogServices/PostCodes/Actions/"
                  "LogService.ClearLog/")
-        // The following privilege is incorrect;  It should be ConfigureManager
-        //.privileges(redfish::privileges::postLogService)
-        .privileges({{"ConfigureComponents"}})
+        .privileges(redfish::privileges::
+                        postLogServiceSubOverComputerSystemLogServiceCollection)
         .methods(boost::beast::http::verb::post)(
             [](const crow::Request&,
                const std::shared_ptr<bmcweb::AsyncResp>& asyncResp) {
