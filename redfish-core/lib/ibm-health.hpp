cbf86938cb88e4608f0e30d65bd89b32ed0796eb
George Liu <liuxiwei@inspur.com>
2021-08-12 16:07:09 +0800

    Add ibm-health.hpp to IBM-OpenBMC
    The original health.hpp can not meet IBM's Health Rollup function,
    so we need to add a new ibm-health.hpp to meet IBM's needs.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>

---
+++ redfish-core/lib/ibm-health.hpp
@@ -0,0 +1,185 @@
+/*
+// Copyright (c) 2019 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+#pragma once
+
+#include "async_resp.hpp"
+
+#include <app.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/container/flat_set.hpp>
+#include <dbus_singleton.hpp>
+
+#include <variant>
+
+namespace redfish
+{
+
+struct ibmHealthPopulate : std::enable_shared_from_this<ibmHealthPopulate>
+{
+    ibmHealthPopulate(const std::shared_ptr<bmcweb::AsyncResp>& asyncRespIn) :
+        asyncResp(asyncRespIn), jsonStatus(asyncResp->res.jsonValue["Status"])
+    {}
+
+    ibmHealthPopulate(const std::shared_ptr<bmcweb::AsyncResp>& asyncRespIn,
+                      nlohmann::json& status) :
+        asyncResp(asyncRespIn),
+        jsonStatus(status)
+    {}
+
+    ~ibmHealthPopulate()
+    {
+        nlohmann::json& health = jsonStatus["Health"];
+        nlohmann::json& rollup = jsonStatus["HealthRollup"];
+
+        health = "OK";
+        rollup = "OK";
+
+        for (const auto& status : statuses)
+        {
+            bool isSelf =
+                selfPath ? boost::starts_with(path.str, *selfPath) : false;
+
+            if (!isSelf)
+            {
+                std::vector<std::string> values;
+                for (const std::string& child : inventory)
+                {
+                    auto serviceNames = status.find(child);
+                    const std::string& path = serviceNames[0];
+                    const std::string& serviceName = serviceNames[1].first;
+                    crow::connections::systemBus->async_method_call(
+                        [asyncResp, &values](const boost::system::error_code ec,
+                                             const std::variant<bool>& health) {
+                            if (ec)
+                            {
+                                BMCWEB_LOG_DEBUG << "Can't get Fan health!";
+                                messages::internalError(asyncResp->res);
+                                return;
+                            }
+
+                            const bool* value = std::get_if<bool>(&health);
+                            const std::string& verifyString = "verify";
+                            if (value == nullptr)
+                            {
+                                messages::internalError(asyncResp->res);
+                                return;
+                            }
+                            if (*value == false)
+                            {
+                                values.push_back(verifyString);
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        },
+                        serviceName, path, "org.freedesktop.DBus.Properties",
+                        "Get",
+                        "xyz.openbmc_project.State.Decorator.OperationalStatus",
+                        "Functional");
+                }
+                if (!values.empty)
+                {
+                    health = "Critical";
+                    rollup = "Critical";
+                    return;
+                }
+            }
+            else
+            {
+                auto serviceNames = status.find(child);
+                const std::string& path = serviceNames[0];
+                const std::string& serviceName = serviceNames[1].first;
+                crow::connections::systemBus->async_method_call(
+                    [asyncResp](const boost::system::error_code ec,
+                                const std::variant<bool>& health) {
+                        if (ec)
+                        {
+                            BMCWEB_LOG_DEBUG << "Can't get Fan health!";
+                            messages::internalError(asyncResp->res);
+                            return;
+                        }
+
+                        const bool* value = std::get_if<bool>(&health);
+                        if (value == nullptr)
+                        {
+                            messages::internalError(asyncResp->res);
+                            return;
+                        }
+                        if (*value == false)
+                        {
+                            health = "Critical";
+                            rollup = "Critical";
+                            return;
+                        }
+                    },
+                    serviceName, path, "org.freedesktop.DBus.Properties", "Get",
+                    "xyz.openbmc_project.State.Decorator.OperationalStatus",
+                    "Functional");
+            }
+        }
+    }
+
+    // this should only be called once per url, others should get updated by
+    // being added as children to the 'main' health object for the page
+    void populate()
+    {
+        if (populated)
+        {
+            return;
+        }
+        populated = true;
+        getAllStatus();
+    }
+
+    void getAllStatus()
+    {
+        std::shared_ptr<ibmHealthPopulate> self = shared_from_this();
+        crow::connections::systemBus->async_method_call(
+            [self](const boost::system::error_code ec,
+                   const std::pair<std::string,
+                                   std::vector<std::pair<
+                                       std::string, std::vector<std::string>>>>&
+                       resp) {
+                if (ec)
+                {
+                    return;
+                }
+                self->statuses = std::move(resp);
+            },
+            "xyz.openbmc_project.ObjectMapper",
+            "/xyz/openbmc_project/object_mapper",
+            "xyz.openbmc_project.ObjectMapper", "GetSubTree", "/", 0,
+            std::array<
+                const char*,
+                1> "xyz.openbmc_project.State.Decorator.OperationalStatus");
+    }
+
+    std::shared_ptr<bmcweb::AsyncResp> asyncResp;
+    nlohmann::json& jsonStatus;
+
+    // self is used if health is for an individual items status, as this is the
+    // 'lowest most' item, the rollup will equal the health
+    std::optional<std::string> selfPath;
+
+    std::vector<std::string> inventory;
+    const std::pair<
+        std::string,
+        std::vector<std::pair<std::string, std::vector<std::string>>>>
+        statuses;
+    bool populated = false;
+};
+} // namespace redfish
