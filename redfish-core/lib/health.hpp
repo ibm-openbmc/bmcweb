919e639a0765f43edb86d702402438d1cd0fc8be
Chicago Duan <duanzhijia01@inspur.com>
2021-09-04 10:01:37 +0800

    Make the Health of chassis unaffected by child inventories
    Tested:
    We need add the xyz.openbmc_project.Inventory.Item.Global interface to the chassis.
    I create a critical association of powersupply1 using the same method as phosphor-logging:
    https://github.com/openbmc/phosphor-logging/blob/76198a2ea2e3deb14078e0d3dd3932b657ddd9b8/extensions/openpower-pels/service_indicators.cpp#L83
    
    Then the chassis HealthRollup will become critical:
    curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Chassis/chassis
    "Status": {
    "Health": "OK",
    "HealthRollup": "Critical",
    "State": "Enabled"
    },
    
    Signed-off-by: Chicago Duan <duanzhijia01@inspur.com>

--- redfish-core/lib/health.hpp
+++ redfish-core/lib/health.hpp
@@ -119,7 +119,6 @@ struct HealthPopulate : std::enable_shared_from_this<HealthPopulate>
             if (boost::starts_with(path.str, globalInventoryPath) &&
                 boost::ends_with(path.str, "critical"))
             {
-                health = "Critical";
                 rollup = "Critical";
                 return;
             }
