22f81d0fbda4ba83e7052e1d1e1875dae5c5cc1a
Ravi Teja <raviteja28031990@gmail.com>
2021-09-27 06:17:24 -0500

    VMI Cert: Increase wait timeout for client certificate

--- include/ibm/management_console_rest.hpp
+++ include/ibm/management_console_rest.hpp
@@ -759,6 +759,8 @@ inline void
                     "Empty Client Certificate";
                 return;
             }
+
+            BMCWEB_LOG_INFO << "Successfully got VMI client certificate";
             asyncResp->res.jsonValue["Certificate"] = *cert;
         },
         "xyz.openbmc_project.Certs.ca.authority.Manager",
@@ -833,7 +835,7 @@ void handleCsrRequest(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
         std::make_shared<boost::asio::steady_timer>(
             crow::connections::systemBus->get_io_context());
 
-    timeout->expires_after(std::chrono::seconds(5));
+    timeout->expires_after(std::chrono::seconds(60));
     crow::connections::systemBus->async_method_call(
         [asyncResp, timeout](const boost::system::error_code ec,
                              sdbusplus::message::message& m) {
@@ -849,6 +851,7 @@ void handleCsrRequest(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                 return;
             }
 
+            BMCWEB_LOG_INFO << "Created CSR Entry object";
             std::string entryId = objPath.filename();
             if (entryId.empty())
             {
@@ -879,10 +882,11 @@ void handleCsrRequest(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                 }
 
                 timeout->cancel();
+                asyncResp->res.addHeader("Retry-After", "60");
                 asyncResp->res.result(
-                    boost::beast::http::status::internal_server_error);
-                asyncResp->res.jsonValue["Description"] =
-                    "Timed out waiting for HostInterface to serve request";
+                    boost::beast::http::status::service_unavailable);
+                BMCWEB_LOG_INFO
+                    << "Timed out waiting for HostInterface to serve request";
             };
 
             timeout->async_wait(timeoutHandler);
@@ -900,7 +904,8 @@ void handleCsrRequest(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                     auto findStatus = values.find("Status");
                     if (findStatus != values.end())
                     {
-                        BMCWEB_LOG_DEBUG << "found Status prop change";
+                        BMCWEB_LOG_INFO
+                            << "Found status prop change of VMI cert object";
                         getCSREntryAck(asyncResp, entryId);
                         timeout->cancel();
                     }
