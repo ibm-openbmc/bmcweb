f9dcf7a6bfde0e0c1c655cdc661c03d45d610fe6
Andrew Geissler <geissonator@yahoo.com>
2021-11-02 20:32:28 -0500

    clang13: clean up file
    Looks like since the previous commit touched this file, it now needs to
    pass the clang13 validation.
    
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>

--- redfish-core/lib/systems.hpp
+++ redfish-core/lib/systems.hpp
@@ -3140,114 +3140,120 @@ inline void requestRoutesSystemActionsReset(App& app)
     BMCWEB_ROUTE(app,
                  "/redfish/v1/Systems/system/Actions/ComputerSystem.Reset/")
         .privileges(redfish::privileges::postComputerSystem)
-        .methods(
-            boost::beast::http::verb::
-                post)([](const crow::Request& req,
-                         const std::shared_ptr<bmcweb::AsyncResp>& asyncResp) {
-            std::string resetType;
-            if (!json_util::readJson(req, asyncResp->res, "ResetType",
-                                     resetType))
-            {
-                return;
-            }
+        .methods(boost::beast::http::verb::post)(
+            [](const crow::Request& req,
+               const std::shared_ptr<bmcweb::AsyncResp>& asyncResp) {
+                std::string resetType;
+                if (!json_util::readJson(req, asyncResp->res, "ResetType",
+                                         resetType))
+                {
+                    return;
+                }
 
-            // Get the command and host vs. chassis
-            std::string command;
-            bool hostCommand;
-            if ((resetType == "On") || (resetType == "ForceOn"))
-            {
-                command = "xyz.openbmc_project.State.Host.Transition.On";
-                hostCommand = true;
-            }
-            else if (resetType == "ForceOff")
-            {
-                command = "xyz.openbmc_project.State.Chassis.Transition.Off";
-                hostCommand = false;
-            }
-            else if (resetType == "GracefulShutdown")
-            {
-                command = "xyz.openbmc_project.State.Host.Transition.Off";
-                hostCommand = true;
-            }
-            else if (resetType == "GracefulRestart")
-            {
-                command = "xyz.openbmc_project.State.Host.Transition."
-                          "GracefulWarmReboot";
-                hostCommand = true;
-            }
-            else if (resetType == "PowerCycle")
-            {
-                command = "xyz.openbmc_project.State.Host.Transition.Reboot";
-                hostCommand = true;
-            }
-            else if (resetType == "Nmi")
-            {
-                doNMI(asyncResp);
-                return;
-            }
-            else
-            {
-                messages::actionParameterUnknown(asyncResp->res, "Reset",
-                                                 resetType);
-                return;
-            }
+                // Get the command and host vs. chassis
+                std::string command;
+                bool hostCommand;
+                if ((resetType == "On") || (resetType == "ForceOn"))
+                {
+                    command = "xyz.openbmc_project.State.Host.Transition.On";
+                    hostCommand = true;
+                }
+                else if (resetType == "ForceOff")
+                {
+                    command =
+                        "xyz.openbmc_project.State.Chassis.Transition.Off";
+                    hostCommand = false;
+                }
+                else if (resetType == "GracefulShutdown")
+                {
+                    command = "xyz.openbmc_project.State.Host.Transition.Off";
+                    hostCommand = true;
+                }
+                else if (resetType == "GracefulRestart")
+                {
+                    command = "xyz.openbmc_project.State.Host.Transition."
+                              "GracefulWarmReboot";
+                    hostCommand = true;
+                }
+                else if (resetType == "PowerCycle")
+                {
+                    command =
+                        "xyz.openbmc_project.State.Host.Transition.Reboot";
+                    hostCommand = true;
+                }
+                else if (resetType == "Nmi")
+                {
+                    doNMI(asyncResp);
+                    return;
+                }
+                else
+                {
+                    messages::actionParameterUnknown(asyncResp->res, "Reset",
+                                                     resetType);
+                    return;
+                }
 
-            if (hostCommand)
-            {
-                crow::connections::systemBus->async_method_call(
-                    [asyncResp, resetType](const boost::system::error_code ec) {
-                        if (ec)
-                        {
-                            BMCWEB_LOG_ERROR << "D-Bus responses error: " << ec;
-                            if (ec.value() ==
-                                boost::asio::error::invalid_argument)
+                if (hostCommand)
+                {
+                    crow::connections::systemBus->async_method_call(
+                        [asyncResp,
+                         resetType](const boost::system::error_code ec) {
+                            if (ec)
                             {
-                                messages::actionParameterNotSupported(
-                                    asyncResp->res, resetType, "Reset");
+                                BMCWEB_LOG_ERROR << "D-Bus responses error: "
+                                                 << ec;
+                                if (ec.value() ==
+                                    boost::asio::error::invalid_argument)
+                                {
+                                    messages::actionParameterNotSupported(
+                                        asyncResp->res, resetType, "Reset");
+                                }
+                                else
+                                {
+                                    messages::internalError(asyncResp->res);
+                                }
+                                return;
                             }
-                            else
+                            messages::success(asyncResp->res);
+                        },
+                        "xyz.openbmc_project.State.Host",
+                        "/xyz/openbmc_project/state/host0",
+                        "org.freedesktop.DBus.Properties", "Set",
+                        "xyz.openbmc_project.State.Host",
+                        "RequestedHostTransition",
+                        std::variant<std::string>{command});
+                }
+                else
+                {
+                    crow::connections::systemBus->async_method_call(
+                        [asyncResp,
+                         resetType](const boost::system::error_code ec) {
+                            if (ec)
                             {
-                                messages::internalError(asyncResp->res);
+                                BMCWEB_LOG_ERROR << "D-Bus responses error: "
+                                                 << ec;
+                                if (ec.value() ==
+                                    boost::asio::error::invalid_argument)
+                                {
+                                    messages::actionParameterNotSupported(
+                                        asyncResp->res, resetType, "Reset");
+                                }
+                                else
+                                {
+                                    messages::internalError(asyncResp->res);
+                                }
+                                return;
                             }
-                            return;
-                        }
-                        messages::success(asyncResp->res);
-                    },
-                    "xyz.openbmc_project.State.Host",
-                    "/xyz/openbmc_project/state/host0",
-                    "org.freedesktop.DBus.Properties", "Set",
-                    "xyz.openbmc_project.State.Host", "RequestedHostTransition",
-                    std::variant<std::string>{command});
-            }
-            else
-            {
-                crow::connections::systemBus->async_method_call(
-                    [asyncResp, resetType](const boost::system::error_code ec) {
-                        if (ec)
-                        {
-                            BMCWEB_LOG_ERROR << "D-Bus responses error: " << ec;
-                            if (ec.value() ==
-                                boost::asio::error::invalid_argument)
-                            {
-                                messages::actionParameterNotSupported(
-                                    asyncResp->res, resetType, "Reset");
-                            }
-                            else
-                            {
-                                messages::internalError(asyncResp->res);
-                            }
-                            return;
-                        }
-                        messages::success(asyncResp->res);
-                    },
-                    "xyz.openbmc_project.State.Chassis",
-                    "/xyz/openbmc_project/state/chassis0",
-                    "org.freedesktop.DBus.Properties", "Set",
-                    "xyz.openbmc_project.State.Chassis",
-                    "RequestedPowerTransition",
-                    std::variant<std::string>{command});
-            }
-        });
+                            messages::success(asyncResp->res);
+                        },
+                        "xyz.openbmc_project.State.Chassis",
+                        "/xyz/openbmc_project/state/chassis0",
+                        "org.freedesktop.DBus.Properties", "Set",
+                        "xyz.openbmc_project.State.Chassis",
+                        "RequestedPowerTransition",
+                        std::variant<std::string>{command});
+                }
+            });
 }
 
 /**
