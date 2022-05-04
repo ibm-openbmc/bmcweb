44a4ee496f987d2c1450faba9b2c84e5bb8d2336
Shawn McCarney <46543807+smccarney@users.noreply.github.com>
2022-05-04 15:46:08 -0500

    PowerSubsystem: Add Allocation properties (#334)
    * PowerSubsystem: Add Allocation properties
    
    Add the Allocation properties to the PowerSubsystem output.
    
    Set the Redfish properties based on properties from the D-Bus
    xyz.openbmc_project.Control.Power.Cap interface:
    * Redfish AllocatedWatts property:
      * Set to PowerCap property if PowerCapEnable is true
      * Set to MaxPowerCapValue property if PowerCapEnable is false
    * Redfish RequestedWatts property:
      * Set to MaxPowerCapValue property
    
    Tested:
    * Passed the Redfish Validator
    * For detailed test plan, see
      https://gist.github.com/smccarney/cadb51e27e97d669b46426b91bd5f659
    
    Example Output:
    $ curl -k https://${bmc}/redfish/v1/Chassis/chassis/PowerSubsystem
    {
      "@odata.id": "/redfish/v1/Chassis/chassis/PowerSubsystem",
      "@odata.type": "#PowerSubsystem.v1_0_0.PowerSubsystem",
      "Allocation": {
        "AllocatedWatts": 5020,
        "RequestedWatts": 6111
      },
      "Id": "PowerSubsystem",
      "Name": "Power Subsystem for Chassis",
      "PowerSupplies": {
        "@odata.id": "/redfish/v1/Chassis/chassis/PowerSubsystem/PowerSupplies"
      }
    }
    
    Signed-off-by: Shawn McCarney <shawnmm@us.ibm.com>
    Change-Id: I764582e1b72b12eaef48ad9973417f6757192541
    
    * PowerSubsystem: Ignore default MaxPowerCapValue
    
    The Redfish Allocation properties in the PowerSubsystem are set based on
    properties of the D-Bus xyz.openbmc_project.Control.Power.Cap interface.
    
    The MaxPowerCapValue property on that interface defaults to a value of
    UINT32_MAX.  Thus, that is the value before the actual maximum power cap
    has been determined.
    
    Update the PowerSubsystem implementation to treat the MaxPowerCapValue
    value as invalid if it equals UINT32_MAX.  This avoids erroneous and
    confusing Redfish output.
    
    Tested:
    * Test where MaxPowerCapValue is invalid
      * Value is 0
      * Value is UINT32_MAX
    * Test where MaxPowerCapValue is valid
    
    Signed-off-by: Shawn McCarney <shawnmm@us.ibm.com>
    Change-Id: I8c6fccb24c456310a13581968fdcfc2a53e94c97
    
    * PowerSubystem: Conform to coding conventions
    
    Update recent enhancements for PowerSubystem Allocation properties so
    they conform to bmcweb coding conventions.
    
    Signed-off-by: Shawn McCarney <shawnmm@us.ibm.com>
    Change-Id: I8b99efea1f56ed1dc75fe223533c5301daf5135c

--- redfish-core/lib/power_subsystem.hpp
+++ redfish-core/lib/power_subsystem.hpp
@@ -1,11 +1,129 @@
 #pragma once
 
 #include <app.hpp>
+#include <dbus_utility.hpp>
 #include <utils/chassis_utils.hpp>
 #include <utils/json_utils.hpp>
 
+#include <cstdint>
+
 namespace redfish
 {
+
+// PowerCap interface
+constexpr auto powerCapInterface = "xyz.openbmc_project.Control.Power.Cap";
+
+inline void getPowerSubsystemAllocationProperties(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& service, const std::string& objectPath)
+{
+    // Get all properties of PowerCap D-Bus interface
+    crow::connections::systemBus->async_method_call(
+        [asyncResp](const boost::system::error_code ec,
+                    const dbus::utility::DBusPropertiesMap& properties) {
+            if (ec)
+            {
+                BMCWEB_LOG_DEBUG << "D-Bus response error on GetAll " << ec;
+                messages::internalError(asyncResp->res);
+                return;
+            }
+
+            // Get value of PowerCap properties from D-Bus response
+            uint32_t powerCap{0};
+            bool powerCapEnable{false};
+            uint32_t maxPowerCapValue{0};
+            for (const auto& [property, value] : properties)
+            {
+                if (property == "PowerCap")
+                {
+                    const uint32_t* valPtr = std::get_if<uint32_t>(&value);
+                    if (valPtr == nullptr)
+                    {
+                        BMCWEB_LOG_DEBUG << "Unexpected data type for PowerCap";
+                        messages::internalError(asyncResp->res);
+                        return;
+                    }
+                    powerCap = *valPtr;
+                }
+                else if (property == "PowerCapEnable")
+                {
+                    const bool* valPtr = std::get_if<bool>(&value);
+                    if (valPtr == nullptr)
+                    {
+                        BMCWEB_LOG_DEBUG
+                            << "Unexpected data type for PowerCapEnable";
+                        messages::internalError(asyncResp->res);
+                        return;
+                    }
+                    powerCapEnable = *valPtr;
+                }
+                else if (property == "MaxPowerCapValue")
+                {
+                    const uint32_t* valPtr = std::get_if<uint32_t>(&value);
+                    if (valPtr == nullptr)
+                    {
+                        BMCWEB_LOG_DEBUG
+                            << "Unexpected data type for MaxPowerCapValue";
+                        messages::internalError(asyncResp->res);
+                        return;
+                    }
+                    maxPowerCapValue = *valPtr;
+                }
+            }
+
+            // If MaxPowerCapValue valid, store Allocation properties in JSON
+            if ((maxPowerCapValue > 0) && (maxPowerCapValue < UINT32_MAX))
+            {
+                if (powerCapEnable)
+                {
+                    asyncResp->res.jsonValue["Allocation"]["AllocatedWatts"] =
+                        powerCap;
+                }
+                else
+                {
+                    asyncResp->res.jsonValue["Allocation"]["AllocatedWatts"] =
+                        maxPowerCapValue;
+                }
+                asyncResp->res.jsonValue["Allocation"]["RequestedWatts"] =
+                    maxPowerCapValue;
+            }
+        },
+        service, objectPath, "org.freedesktop.DBus.Properties", "GetAll",
+        powerCapInterface);
+}
+
+inline void getPowerSubsystemAllocation(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+{
+    // Find service and object path that implement PowerCap interface (if any)
+    crow::connections::systemBus->async_method_call(
+        [asyncResp](const boost::system::error_code ec,
+                    const dbus::utility::MapperGetSubTreeResponse& subTree) {
+            if (ec)
+            {
+                BMCWEB_LOG_DEBUG << "D-Bus response error on GetSubTree " << ec;
+                messages::internalError(asyncResp->res);
+                return;
+            }
+
+            if (!subTree.empty())
+            {
+                const auto& [objectPath, serviceMap] = subTree[0];
+                if (!serviceMap.empty())
+                {
+                    const auto& service = serviceMap[0].first;
+
+                    // Get properties from PowerCap interface and store in JSON
+                    getPowerSubsystemAllocationProperties(asyncResp, service,
+                                                          objectPath);
+                }
+            }
+        },
+        "xyz.openbmc_project.ObjectMapper",
+        "/xyz/openbmc_project/object_mapper",
+        "xyz.openbmc_project.ObjectMapper", "GetSubTree", "/", 0,
+        std::array<const char*, 1>{powerCapInterface});
+}
 
 inline void
     getPowerSubsystem(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
@@ -23,6 +141,9 @@ inline void
         "/redfish/v1/Chassis/" + chassisID + "/PowerSubsystem";
     asyncResp->res.jsonValue["PowerSupplies"]["@odata.id"] =
         "/redfish/v1/Chassis/" + chassisID + "/PowerSubsystem/PowerSupplies";
+
+    // Get Allocation information from D-Bus and store in JSON
+    getPowerSubsystemAllocation(asyncResp);
 }
 
 inline void requestRoutesPowerSubsystem(App& app)
