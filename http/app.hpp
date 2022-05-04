0202fa34af19266716baac1db91c8cb63c20e825
patelabhishek9893 <82394680+patelabhishek9893@users.noreply.github.com>
2022-05-04 14:32:53 -0500

    Add asyncResp support during handleUpgrade (#322)
    The current implementation uses the earlier method of using the response
    object and calling response.end() to initiate completion handler.
    This commit modifies the implementation to use asyncResp, where the
    completion handler gets called asynchronously as the response object
    goes out of scope.
    
    Co-authored-by: Abhishek Patel <Abhishek.Patel@ibm.com>

--- http/app.hpp
+++ http/app.hpp
@@ -45,9 +45,11 @@ class App
     }
 
     template <typename Adaptor>
-    void handleUpgrade(const Request& req, Response& res, Adaptor&& adaptor)
+    void handleUpgrade(const Request& req,
+                       std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                       Adaptor&& adaptor)
     {
-        router.handleUpgrade(req, res, std::move(adaptor));
+        router.handleUpgrade(req, asyncResp, std::forward<Adaptor>(adaptor));
     }
 
     void handle(Request& req,
