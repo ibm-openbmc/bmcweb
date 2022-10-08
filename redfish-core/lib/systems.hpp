cf06d415a58149a79e90dc45713dce4350dd6f85
George Liu <liuxiwei@inspur.com>
2022-10-08 09:27:47 +0800

    Update Location Indicator Active property
    When Redfish get/set Location Indicator Active property, it should
    first find the corresponding ledGroup through the identify[1]
    association, and then get/set Asserted property, instead of
    hard-coding ledGroup.
    
    [1] https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/58299
    
    Tested: built bmcweb successfully and Validator passes
    1. get LocationIndicatorActive
    curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/
    Systems/system
    {
      "@odata.id": "/redfish/v1/Systems/system",
      "@odata.type": "#ComputerSystem.v1_16_0.ComputerSystem",
      ...
      "LocationIndicatorActive": true,
      ...
    }
    
    2. set LocationIndicatorActive
    curl -k -H "X-Auth-Token: $token" -H "Content-Type: application/json"
    -X PATCH -d '{"LocationIndicatorActive": false}'
    https://${bmc}/redfish/v1/Systems/system
    
    get LocationIndicatorActive
    curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/
    Systems/system
    {
      "@odata.id": "/redfish/v1/Systems/system",
      "@odata.type": "#ComputerSystem.v1_16_0.ComputerSystem",
      ...
      "LocationIndicatorActive": false,
      ...
    }
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I0c5d538c1dd9f0d8dd1e6ae4d8ba4f606b804373

--- redfish-core/lib/systems.hpp
+++ redfish-core/lib/systems.hpp
@@ -20,6 +20,7 @@
 #include "pcie.hpp"
 #include "query.hpp"
 #include "redfish_util.hpp"
+#include "utils/systems_utils.hpp"
 #include "utils/time_utils.hpp"
 
 #include <app.hpp>
@@ -2394,7 +2395,19 @@ inline void requestRoutesSystems(App& app)
             aRsp->res.jsonValue["Links"]["Chassis"] = std::move(chassisArray);
         });
 
-        getLocationIndicatorActive(asyncResp);
+        redfish::systems_utils::getValidSystemsPath(
+            asyncResp, systemName,
+            [asyncResp,
+             systemName](const std::optional<std::string>& validSystemsPath) {
+            if (!validSystemsPath)
+            {
+                messages::resourceNotFound(asyncResp->res, "Systems",
+                                           systemName);
+                return;
+            }
+            getLocationIndicatorActive(asyncResp, *validSystemsPath);
+            });
+
         // TODO (Gunnar): Remove IndicatorLED after enough time has passed
         getIndicatorLedState(asyncResp);
         getComputerSystem(asyncResp);
@@ -2497,7 +2510,20 @@ inline void requestRoutesSystems(App& app)
 
         if (locationIndicatorActive)
         {
-            setLocationIndicatorActive(asyncResp, *locationIndicatorActive);
+            redfish::systems_utils::getValidSystemsPath(
+                asyncResp, systemName,
+                [asyncResp, systemName,
+                 locationIndicatorActive{*locationIndicatorActive}](
+                    const std::optional<std::string>& validSystemsPath) {
+                if (!validSystemsPath)
+                {
+                    messages::resourceNotFound(asyncResp->res, "Systems",
+                                               systemName);
+                    return;
+                }
+                setLocationIndicatorActive(asyncResp, *validSystemsPath,
+                                           locationIndicatorActive);
+                });
         }
 
         // TODO (Gunnar): Remove IndicatorLED after enough time has
