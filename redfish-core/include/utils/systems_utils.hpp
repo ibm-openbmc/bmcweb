0ce343250aa85b9a3dc5ac858b76be8ba939fa54
George Liu <liuxiwei@inspur.com>
2022-10-08 09:27:47 +0800

    Update Chassis Location Indicator Active property
    Modify get/set of LocationIndicatorActive property for Chassis and
    Systems to use identifying association instead of hard-coding
    ledGroup. [1]
    
    [1] https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/58299
    
    Tested: built bmcweb successfully and Validator passes
    ```
    1. Get for Systems
    curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Systems/system
    {
      "@odata.id": "/redfish/v1/Systems/system",
      "@odata.type": "#ComputerSystem.v1_22_0.ComputerSystem",
      ...
      "LocationIndicatorActive": true,
      ...
    }
    
    2. Set for Systems
    curl -k -H "X-Auth-Token: $token" -H "Content-Type: application/json" -X PATCH -d '{"LocationIndicatorActive": false}' https://${bmc}/redfish/v1/Systems/system
    
    curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Systems/system
    {
      "@odata.id": "/redfish/v1/Systems/system",
      "@odata.type": "#ComputerSystem.v1_22_0.ComputerSystem",
      ...
      "LocationIndicatorActive": false,
      ...
    }
    
    3. Get for Chassis
    curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Chassis/chassis
    {
      "@odata.id": "/redfish/v1/Chassis/chassis",
      "@odata.type": "#Chassis.v1_22_0.Chassis",
      ...
        "LocationIndicatorActive": true,
      ...
    }
    
    4. Set for Chassis
    curl -k -H "X-Auth-Token: $token" -H "Content-Type: application/json" -X PATCH -d '{"LocationIndicatorActive":false}' https://${bmc}/redfish/v1/Chassis/chassis
    
    curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Chassis/chassis
    {
      "@odata.id": "/redfish/v1/Chassis/chassis",
      "@odata.type": "#Chassis.v1_22_0.Chassis",
      ...
      "LocationIndicatorActive": false,
      ...
    }
    
    5. Get for Mex
    curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Chassis/chassis15363
    {
      "@odata.id": "/redfish/v1/Chassis/chassis15363",
      "@odata.type": "#Chassis.v1_22_0.Chassis",
      ...
      "LocationIndicatorActive": false,
      ...
    }
    
    6. Set for Mex
    curl -k -H "X-Auth-Token: $token" -H "Content-Type: application/json" -X PATCH -d '{"LocationIndicatorActive":true}' https://${bmc}/redfish/v1/Chassis/chassis15363
    
    curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Chassis/chassis15363
    {
      "@odata.id": "/redfish/v1/Chassis/chassis15363",
      "@odata.type": "#Chassis.v1_22_0.Chassis",
      ...
      "LocationIndicatorActive": true,
      ...
    }
    ```
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Signed-off-by: Janet Adkins <janeta@us.ibm.com>

---
+++ redfish-core/include/utils/systems_utils.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include "async_resp.hpp"
+
+#include <boost/system/error_code.hpp>
+
+#include <array>
+#include <functional>
+#include <optional>
+#include <string>
+#include <utility>
+
+namespace redfish
+{
+
+namespace systems_utils
+{
+static void afterGetValidSystemsPath(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& systemId,
+    const std::function<void(const std::optional<std::string>&)>& callback,
+    const boost::system::error_code& ec,
+    const dbus::utility::MapperGetSubTreePathsResponse& systemsPaths)
+{
+    BMCWEB_LOG_DEBUG("getValidSystemsPath respHandler enter");
+    if (ec)
+    {
+        if (ec.value() == EBADR)
+        {
+            BMCWEB_LOG_DEBUG("No systems found");
+            callback("");
+            return;
+        }
+        BMCWEB_LOG_ERROR("getValidSystemsPath respHandler DBUS error: {}", ec);
+        messages::internalError(asyncResp->res);
+        return;
+    }
+
+    for (const std::string& system : systemsPaths)
+    {
+        sdbusplus::message::object_path path(system);
+        if (path.filename() == systemId)
+        {
+            callback(path);
+            return;
+        }
+    }
+    BMCWEB_LOG_DEBUG("No system named {} found", systemId);
+    callback("");
+}
+
+inline void getValidSystemsPath(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& systemId,
+    std::function<void(const std::optional<std::string>&)>&& callback)
+{
+    BMCWEB_LOG_DEBUG("checkSystemsId enter");
+
+    // Get the Systems Collection
+    constexpr std::array<std::string_view, 1> interfaces = {
+        "xyz.openbmc_project.Inventory.Item.System"};
+    dbus::utility::getSubTreePaths(
+        "/xyz/openbmc_project/inventory", 0, interfaces,
+        [asyncResp, systemId, callback{std::move(callback)}](
+            const boost::system::error_code& ec,
+            const dbus::utility::MapperGetSubTreePathsResponse& systemsPaths) {
+            afterGetValidSystemsPath(asyncResp, systemId, callback, ec,
+                                     systemsPaths);
+            return;
+        });
+}
+
+} // namespace systems_utils
+} // namespace redfish
--- redfish-core/lib/systems.hpp
+++ redfish-core/lib/systems.hpp
@@ -37,6 +37,7 @@
 #include "utils/json_utils.hpp"
 #include "utils/pcie_util.hpp"
 #include "utils/sw_utils.hpp"
+#include "utils/systems_utils.hpp"
 #include "utils/time_utils.hpp"
 
 #include <boost/asio/error.hpp>
@@ -2852,7 +2853,16 @@ inline void
             aRsp->res.jsonValue["Links"]["Chassis"] = std::move(chassisArray);
         });
 
-    getSystemLocationIndicatorActive(asyncResp);
+    systems_utils::getValidSystemsPath(
+        asyncResp, systemName,
+        [asyncResp,
+         systemName](const std::optional<std::string>& validSystemsPath) {
+            if (validSystemsPath)
+            {
+                getLocationIndicatorActive(asyncResp, *validSystemsPath);
+            }
+        });
+
     // TODO (Gunnar): Remove IndicatorLED after enough time has passed
     getIndicatorLedState(asyncResp);
     getComputerSystem(asyncResp);
@@ -2996,7 +3006,20 @@ inline void handleComputerSystemPatch(
 
     if (locationIndicatorActive)
     {
-        setSystemLocationIndicatorActive(asyncResp, *locationIndicatorActive);
+        systems_utils::getValidSystemsPath(
+            asyncResp, systemName,
+            [asyncResp, systemName,
+             locationIndicatorActive{*locationIndicatorActive}](
+                const std::optional<std::string>& validSystemsPath) {
+                if (!validSystemsPath)
+                {
+                    messages::resourceNotFound(asyncResp->res, "Systems",
+                                               systemName);
+                    return;
+                }
+                setLocationIndicatorActive(asyncResp, *validSystemsPath,
+                                           locationIndicatorActive);
+            });
     }
 
     // TODO (Gunnar): Remove IndicatorLED after enough time has
