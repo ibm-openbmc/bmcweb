5627bdad176436840868dd29a744365af631b4af
deepakala-k <112156780+deepakala-k@users.noreply.github.com>
2023-01-06 21:28:13 +0530

    Add missing odata.id field under OriginOfCondition for assemblies (#494)
    The OriginOfCondition Link should have an odata.id which was missing for
    assemblies. All other Deconfig records already correctly had this property.
    
    curl -k -H "X-Auth-Token: $bmc_token" -XGET
    https://${BMC_IP}/redfish/v1/Systems/system/LogServices/HardwareIsolation/Entries/50
    {
      "@odata.id":
    "/redfish/v1/Systems/system/LogServices/HardwareIsolation/Entries/50",
      "@odata.type": "#LogEntry.v1_9_0.LogEntry",
      "Created": "2023-01-03T10:38:43+00:00",
      "EntryType": "Event",
      "Id": "50",
      "Links": {
        "OriginOfCondition": "/redfish/v1/Chassis/chassis/Assembly#/Assemblies/49"
    <-- missing @odata.id
      },
      "Message": "TPM Card",
      "Name": "Hardware Isolation Entry",
      "Severity": "OK"
    }
    
    curl -k -H "X-Auth-Token: $bmc_token" -XGET
    https://${BMC_IP}/redfish/v1/Systems/system/LogServices/HardwareIsolation/Entries/50
    {
      "@odata.id":
    "/redfish/v1/Systems/system/LogServices/HardwareIsolation/Entries/50",
      "@odata.type": "#LogEntry.v1_9_0.LogEntry",
      "Created": "2023-01-03T10:38:43+00:00",
      "EntryType": "Event",
      "Id": "50",
      "Links": {
        "OriginOfCondition": {
          "@odata.id": "/redfish/v1/Chassis/chassis/Assembly#/Assemblies/49" <--
    odata.id present
        }
      },
      "Message": "TPM Card",
      "Name": "Hardware Isolation Entry",
      "Severity": "OK"
    }
    
    Signed-off-by: deepakala <deepakala.karthikeyan@ibm.com>
    
    Signed-off-by: deepakala <deepakala.karthikeyan@ibm.com>

--- redfish-core/lib/log_services.hpp
+++ redfish-core/lib/log_services.hpp
@@ -5747,6 +5747,7 @@ inline void getRedfishUriByDbusObjPath(
                         {
                             auto uriPropPath = "/Links"_json_pointer;
                             uriPropPath /= "OriginOfCondition";
+                            uriPropPath /= "@odata.id";
 
                             assembly::fillWithAssemblyId(
                                 asyncResp, std::get<0>(assemblyParent),
