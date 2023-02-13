ae56b19a462236c3cb46e4992b0d493266965deb
George Liu <liuxiwei@inspur.com>
2023-02-14 00:47:47 +0800

    Redfish: Implement OEM lamp test and SAI (#541)
    Implement OEM lamp test in redfish/v1/Systems/system
    
    Implement SystemAttentionIndicator in redfish/v1/Systems/system
    refer: ibm-openbmc/dev#2120
    
    We use macros to control the URL, which need to be enabled in
    bmcweb_%.bbappend to see it, for example:
    EXTRA_OEMESON_append = " \
        -Dibm-led-extensions=enabled \
        "
    
    Tested:
    1. Get the lamp test and SAI:
    curl -k -H "X-Auth-Token: $token" \
    https://${bmc}/redfish/v1/Systems/system
    {
      "@odata.id": "/redfish/v1/Systems/system",
      "@odata.type": "#ComputerSystem.v1_16_0.ComputerSystem",
      ...
      "Oem": {
        "@odata.type": "#OemComputerSystem.Oem",
        "IBM": {
          "@odata.type": "#OemComputerSystem.IBM",
          "LampTest": false,
          "PartitionSystemAttentionIndicator": false,
          "PlatformSystemAttentionIndicator": false
        }
      },
      ...
    }
    
    2. Set the lamp test and SAI:
    curl -k -H "X-Auth-Token: $token" -X PATCH \
    https://${bmc}/redfish/v1/Systems/system -d \
    '{"Oem":{"IBM":{"LampTest": true, \
    "PartitionSystemAttentionIndicator": true}}}'
    
    3. Get the lamp test and SAI:
    curl -k -H "X-Auth-Token: $token" \
    https://${bmc}/redfish/v1/Systems/system
    {
      "@odata.id": "/redfish/v1/Systems/system",
      "@odata.type": "#ComputerSystem.v1_16_0.ComputerSystem",
      ...
      "Oem": {
        "@odata.type": "#OemComputerSystem.Oem",
        "IBM": {
          "@odata.type": "#OemComputerSystem.IBM",
          "LampTest": true,
          "PartitionSystemAttentionIndicator": true,
          "PlatformSystemAttentionIndicator": false
        }
      },
      ...
    }
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>

---
+++ redfish-core/lib/oem/ibm/system_attention_indicator.hpp
@@ -0,0 +1,138 @@
+#pragma once
+
+#include "async_resp.hpp"
+#include "dbus_utility.hpp"
+
+#include <variant>
+
+namespace redfish
+{
+/**
+ * @brief Get System Attention Indicator
+ *
+ * @param[in] aResp             Shared pointer for generating response message.
+ * @param[in] propertyValue     The property value
+ *                              (PartitionSystemAttentionIndicator/PlatformSystemAttentionIndicator).
+ *
+ * @return None.
+ */
+inline void getSAI(const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                   const std::string& propertyValue)
+{
+    BMCWEB_LOG_DEBUG << "Get platform/partition system attention indicator";
+
+    std::string name{};
+    if (propertyValue == "PartitionSystemAttentionIndicator")
+    {
+        name = "partition_system_attention_indicator";
+    }
+    else if (propertyValue == "PlatformSystemAttentionIndicator")
+    {
+        name = "platform_system_attention_indicator";
+    }
+    else
+    {
+        messages::propertyUnknown(aResp->res, propertyValue);
+        return;
+    }
+
+    std::array<const char*, 1> interfaces = {"xyz.openbmc_project.Led.Group"};
+    dbus::utility::getDbusObject(
+        "/xyz/openbmc_project/led/groups/" + name, interfaces,
+        [aResp, name,
+         propertyValue](const boost::system::error_code& ec,
+                        const dbus::utility::MapperGetObject& object) {
+        if (ec || object.empty())
+        {
+            BMCWEB_LOG_ERROR << "DBUS response error " << ec.message();
+            messages::internalError(aResp->res);
+            return;
+        }
+
+        sdbusplus::asio::getProperty<bool>(
+            *crow::connections::systemBus, object.begin()->first,
+            "/xyz/openbmc_project/led/groups/" + name,
+            "xyz.openbmc_project.Led.Group", "Asserted",
+            [aResp, propertyValue](const boost::system::error_code& ec1,
+                                   bool assert) {
+            if (ec1)
+            {
+                if (ec1.value() != EBADR)
+                {
+                    messages::internalError(aResp->res);
+                }
+                return;
+            }
+
+            nlohmann::json& oemSAI = aResp->res.jsonValue["Oem"]["IBM"];
+            oemSAI["@odata.type"] = "#OemComputerSystem.IBM";
+            if (propertyValue == "PartitionSystemAttentionIndicator")
+            {
+                oemSAI["PartitionSystemAttentionIndicator"] = assert;
+            }
+            else if (propertyValue == "PlatformSystemAttentionIndicator")
+            {
+                oemSAI["PlatformSystemAttentionIndicator"] = assert;
+            }
+            });
+        });
+}
+
+/**
+ * @brief Set System Attention Indicator
+ *
+ * @param[in] aResp             Shared pointer for generating response message.
+ * @param[in] propertyValue     The property value
+ *                              (PartitionSystemAttentionIndicator/PlatformSystemAttentionIndicator).
+ * @param[in] value             true or fasle
+ *
+ * @return None.
+ */
+inline void setSAI(const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                   const std::string& propertyValue, bool value)
+{
+    BMCWEB_LOG_DEBUG << "Set platform/partition system attention indicator";
+
+    std::string name{};
+    if (propertyValue == "PartitionSystemAttentionIndicator")
+    {
+        name = "partition_system_attention_indicator";
+    }
+    else if (propertyValue == "PlatformSystemAttentionIndicator")
+    {
+        name = "platform_system_attention_indicator";
+    }
+    else
+    {
+        return;
+    }
+
+    std::array<const char*, 1> interfaces = {"xyz.openbmc_project.Led.Group"};
+    dbus::utility::getDbusObject(
+        "/xyz/openbmc_project/led/groups/" + name, interfaces,
+        [aResp, name, value](const boost::system::error_code& ec,
+                             const dbus::utility::MapperGetObject& object) {
+        if (ec || object.empty())
+        {
+            BMCWEB_LOG_ERROR << "DBUS response error " << ec.message();
+            messages::internalError(aResp->res);
+            return;
+        }
+
+        sdbusplus::asio::setProperty(
+            *crow::connections::systemBus, object.begin()->first,
+            "/xyz/openbmc_project/led/groups/" + name,
+            "xyz.openbmc_project.Led.Group", "Asserted", value,
+            [aResp](const boost::system::error_code& ec1) {
+            if (ec1)
+            {
+                if (ec1.value() != EBADR)
+                {
+                    messages::internalError(aResp->res);
+                }
+                return;
+            }
+            });
+        });
+}
+} // namespace redfish
