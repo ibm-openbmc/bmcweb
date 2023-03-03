4247c0e05fb85e1fac085e5dedb971a48c8f457e
George Liu <liuxiwei@inspur.com>
2023-03-03 14:17:47 +0800

    Implementation of Port schema in bmcweb
    This commit implemets Port and Port collection schema on bmcweb.
    The schema describes a port of a switch, controller, chassis,
    or any other device that could be connected to another entity.
    
    Current use case:
    It is implemented to model collection of CXP ports on fabric
    adapters.
    
    Validator has been executed and no new error has been found.
    
    Sample output:
    curl -k -H "X-Auth-Token: $bmc_token" -X GET https://${bmc}/redfish \
    /v1/Systems/system/FabricAdapters/pcie_cable_card9
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/ \
                    pcie_cable_card9",
      "@odata.type": "#FabricAdapter.v1_4_0.FabricAdapter",
      "Id": "pcie_cable_card9",
      "Name": "pcie_cable_card9",
      "Ports": {
        "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/ \
                      pcie_cable_card9/Ports"
      }
    }
    
    curl -k -H "X-Auth-Token: $bmc_token" -X GET https://${bmc}/redfish \
    /v1/Systems/system/FabricAdapters/pcie_cable_card9/Ports
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/ \
                    pcie_cable_card9/Ports",
      "@odata.type": "#PortCollection.PortCollection",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/ \
                        pcie_cable_card9/Ports/cxp_bot"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/ \
                        pcie_cable_card9/Ports/cxp_top"
        }
      ],
      "Members@odata.count": 2,
      "Name": "Port Collection"
    }
    
    curl -k -H "X-Auth-Token: $bmc_token" -X GET https://${bmc}/redfish \
    /v1/Systems/system/FabricAdapters/pcie_cable_card9/Ports/cxp_bot
    {
      "@odata.id": "/redfish/v1/Systems/system/FabricAdapters/ \
                    pcie_cable_card9/Ports/cxp_bot",
      "@odata.type": "#Port.v1_7_0.Port",
      "Id": "cxp_bot",
      "Location": {
        "PartLocation": {
          "ServiceLabel": "U78DA.ND0.1234567-P0-C9-T1"
        }
      },
      "Name": "cxp_bot"
    }
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>

---
+++ redfish-core/lib/port.hpp
@@ -0,0 +1,317 @@
+#pragma once
+
+#include "app.hpp"
+#include "fabric_adapters.hpp"
+#include "query.hpp"
+#include "registries/privilege_registry.hpp"
+
+#include <utils/json_utils.hpp>
+
+#include <array>
+#include <functional>
+#include <memory>
+#include <string>
+#include <string_view>
+
+namespace redfish
+{
+
+/**
+ * @brief Api to get Port properties.
+ * @param[in,out]   aResp       Async HTTP response.
+ * @param[in]       portInvPath Object path of the Port.
+ * @param[in]       serviceMap  A map to hold Service and corresponding
+ *                              interface list for the given port.
+ */
+inline void getPortProperties(const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                              const std::string& portInvPath,
+                              const dbus::utility::MapperServiceMap& serviceMap)
+{
+    BMCWEB_LOG_DEBUG << "Getting Properties for port " << portInvPath;
+
+    for (const auto& connection : serviceMap)
+    {
+        for (const auto& interface : connection.second)
+        {
+            if (interface ==
+                "xyz.openbmc_project.Inventory.Decorator.LocationCode")
+            {
+                sdbusplus::asio::getProperty<std::string>(
+                    *crow::connections::systemBus, connection.first,
+                    portInvPath, interface, "LocationCode",
+                    [aResp](const boost::system::error_code& ec,
+                            const std::string& value) {
+                    if (ec)
+                    {
+                        BMCWEB_LOG_DEBUG << "DBUS response error";
+                        messages::internalError(aResp->res);
+                        return;
+                    }
+
+                    aResp->res
+                        .jsonValue["Location"]["PartLocation"]["ServiceLabel"] =
+                        value;
+                    });
+            }
+        }
+    }
+}
+
+/**
+ * @brief Api to get collection of FRUs to be modelled as
+ * Port.
+ *
+ * @param[in,out]   aResp              Async HTTP response.
+ * @param[in]       systemName         System name Id.
+ * @param[in]       adapterId          AdapterId whose ports
+ * are to be collected.
+ * @param[in]       interfaces         List of interfaces to
+ * constrain the GetSubTree search
+ */
+inline void getPortCollection(const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                              const std::string& systemName,
+                              const std::string& adapterId)
+{
+    constexpr std::array<std::string_view, 1> interfaces = {
+        "xyz.openbmc_project.Inventory.Item.Connector"};
+    dbus::utility::getSubTreePaths(
+        "/xyz/openbmc_project/inventory", 0, interfaces,
+        [aResp, systemName,
+         adapterId](const boost::system::error_code& ec,
+                    const dbus::utility::MapperGetSubTreePathsResponse& paths) {
+        if (ec)
+        {
+            BMCWEB_LOG_DEBUG << "DBUS response error";
+            messages::internalError(aResp->res);
+            return;
+        }
+        nlohmann::json& members = aResp->res.jsonValue["Members"];
+        members = nlohmann::json::array();
+
+        for (const auto& path : paths)
+        {
+            if (sdbusplus::message::object_path(path)
+                    .parent_path()
+                    .filename() != adapterId)
+            {
+                continue;
+            }
+
+            std::string connectorId =
+                sdbusplus::message::object_path(path).filename();
+            nlohmann::json item;
+            item["@odata.id"] = crow::utility::urlFromPieces(
+                "redfish", "v1", "Systems", systemName, "FabricAdapters",
+                adapterId, "Ports", connectorId);
+            members.emplace_back(std::move(item));
+        }
+        aResp->res.jsonValue["Members@odata.count"] = members.size();
+        });
+}
+
+inline void
+    handlePortCollectionHead(crow::App& app, const crow::Request& req,
+                             const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                             const std::string& systemName,
+                             const std::string& adapterId)
+{
+    if (!redfish::setUpRedfishRoute(app, req, aResp))
+    {
+        return;
+    }
+
+    getValidFabricAdapterPath(adapterId, systemName, aResp,
+                              [aResp](const std::string&, const std::string&,
+                                      const dbus::utility::InterfaceList&) {
+        aResp->res.addHeader(
+            boost::beast::http::field::link,
+            "</redfish/v1/JsonSchemas/PortCollection/PortCollection.json>; rel=describedby");
+    });
+}
+
+inline void doPortCollectionGet(const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                                const std::string& systemName,
+                                const std::string& adapterId)
+{
+    aResp->res.addHeader(
+        boost::beast::http::field::link,
+        "</redfish/v1/JsonSchemas/PortCollection/PortCollection.json>; rel=describedby");
+    aResp->res.jsonValue["@odata.type"] = "#PortCollection.PortCollection";
+    aResp->res.jsonValue["Name"] = "Port Collection";
+    aResp->res.jsonValue["@odata.id"] =
+        crow::utility::urlFromPieces("redfish", "v1", "Systems", systemName,
+                                     "FabricAdapters", adapterId, "Ports");
+
+    getPortCollection(aResp, systemName, adapterId);
+}
+
+inline void
+    handlePortCollectionGet(App& app, const crow::Request& req,
+                            const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                            const std::string& systemName,
+                            const std::string& adapterId)
+{
+    if (!redfish::setUpRedfishRoute(app, req, aResp))
+    {
+        return;
+    }
+
+    getValidFabricAdapterPath(
+        adapterId, systemName, aResp,
+        [aResp, systemName, adapterId](const std::string&, const std::string&,
+                                       const dbus::utility::InterfaceList&) {
+        doPortCollectionGet(aResp, systemName, adapterId);
+        });
+}
+
+/**
+ * Systems derived class for delivering port collection Schema.
+ */
+inline void requestRoutesPortCollection(App& app)
+{
+    BMCWEB_ROUTE(app, "/redfish/v1/Systems/<str>/FabricAdapters/<str>/Ports/")
+        .privileges(redfish::privileges::headPortCollection)
+        .methods(boost::beast::http::verb::head)(
+            std::bind_front(handlePortCollectionHead, std::ref(app)));
+
+    BMCWEB_ROUTE(app, "/redfish/v1/Systems/<str>/FabricAdapters/<str>/Ports/")
+        .privileges(redfish::privileges::getPortCollection)
+        .methods(boost::beast::http::verb::get)(
+            std::bind_front(handlePortCollectionGet, std::ref(app)));
+}
+
+inline void handlePortError(const boost::system::error_code& ec,
+                            crow::Response& res, const std::string& portId)
+{
+
+    if (ec.value() == boost::system::errc::io_error)
+    {
+        messages::resourceNotFound(res, "#Port.v1_7_0.Port", portId);
+        return;
+    }
+
+    BMCWEB_LOG_ERROR << "DBus method call failed with error " << ec.value();
+    messages::internalError(res);
+}
+
+inline bool checkPortId(const std::string& portPath, const std::string& portId)
+{
+    std::string portName = sdbusplus::message::object_path(portPath).filename();
+
+    return !(portName.empty() || portName != portId);
+}
+
+inline void getValidPortPath(
+    const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+    const std::string& adapterPath, const std::string& portId,
+    std::function<void(const std::string& portPath,
+                       const dbus::utility::MapperServiceMap& serviceMap)>&&
+        callback)
+{
+    constexpr std::array<std::string_view, 1> interfaces{
+        "xyz.openbmc_project.Inventory.Item.Connector"};
+
+    dbus::utility::getSubTree(
+        adapterPath, 0, interfaces,
+        [portId, aResp,
+         callback](const boost::system::error_code& ec,
+                   const dbus::utility::MapperGetSubTreeResponse& subtree) {
+        if (ec)
+        {
+            handlePortError(ec, aResp->res, portId);
+            return;
+        }
+        for (const auto& [portPath, serviceMap] : subtree)
+        {
+            if (checkPortId(portPath, portId))
+            {
+                callback(portPath, serviceMap);
+                return;
+            }
+        }
+        BMCWEB_LOG_WARNING << "Port not found";
+        messages::resourceNotFound(aResp->res, "Port", portId);
+        });
+}
+
+inline void handlePortHead(crow::App& app, const crow::Request& req,
+                           const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                           const std::string& systemName,
+                           const std::string& adapterId,
+                           const std::string& portId)
+{
+    if (!redfish::setUpRedfishRoute(app, req, aResp))
+    {
+        return;
+    }
+
+    getValidFabricAdapterPath(
+        adapterId, systemName, aResp,
+        [aResp, portId](const std::string& adapterPath, const std::string&,
+                        const dbus::utility::InterfaceList&) {
+        getValidPortPath(aResp, adapterPath, portId,
+                         [aResp](const std::string&,
+                                 const dbus::utility::MapperServiceMap&) {
+            aResp->res.addHeader(
+                boost::beast::http::field::link,
+                "</redfish/v1/JsonSchemas/Port/Port.json>; rel=describedby");
+        });
+        });
+}
+
+inline void handlePortGet(App& app, const crow::Request& req,
+                          const std::shared_ptr<bmcweb::AsyncResp>& aResp,
+                          const std::string& systemName,
+                          const std::string& adapterId,
+                          const std::string& portId)
+{
+    if (!redfish::setUpRedfishRoute(app, req, aResp))
+    {
+        return;
+    }
+
+    getValidFabricAdapterPath(
+        adapterId, systemName, aResp,
+        [aResp, portId, adapterId,
+         systemName](const std::string& adapterPath, const std::string&,
+                     const dbus::utility::InterfaceList&) {
+        getValidPortPath(
+            aResp, adapterPath, portId,
+            [aResp, adapterId, systemName,
+             portId](const std::string& portPath,
+                     const dbus::utility::MapperServiceMap& serviceMap) {
+            aResp->res.addHeader(
+                boost::beast::http::field::link,
+                "</redfish/v1/JsonSchemas/Port/Port.json>; rel=describedby");
+
+            aResp->res.jsonValue["@odata.type"] = "#Port.v1_7_0.Port";
+            aResp->res.jsonValue["@odata.id"] = crow::utility::urlFromPieces(
+                "redfish", "v1", "Systems", systemName, "FabricAdapters",
+                adapterId, "Ports", portId);
+            aResp->res.jsonValue["Id"] = portId;
+            aResp->res.jsonValue["Name"] = portId;
+
+            getPortProperties(aResp, portPath, serviceMap);
+            });
+        });
+}
+
+/**
+ * Systems derived class for delivering port Schema.
+ */
+inline void requestRoutesPort(App& app)
+{
+    BMCWEB_ROUTE(app,
+                 "/redfish/v1/Systems/<str>/FabricAdapters/<str>/Ports/<str>/")
+        .privileges(redfish::privileges::headPort)
+        .methods(boost::beast::http::verb::head)(
+            std::bind_front(handlePortHead, std::ref(app)));
+
+    BMCWEB_ROUTE(app,
+                 "/redfish/v1/Systems/<str>/FabricAdapters/<str>/Ports/<str>/")
+        .privileges(redfish::privileges::getPort)
+        .methods(boost::beast::http::verb::get)(
+            std::bind_front(handlePortGet, std::ref(app)));
+}
+
+} // namespace redfish
