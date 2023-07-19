4bc3e20fcbb8fc8a58b6f02ba155d397b1b7fd95
Janet Adkins <janeta@us.ibm.com>
2023-07-19 08:41:26 -0500

    Audit Log: Hide details of building audit message into auditEvent() (#725)
    * Audit Log: Hide details of building audit message into auditEvent()
    
    Added utility functions to centralize checking whether detail data for
    the request being logged should be included in the log message. Moved
    the building of the audit message into the audit namespace so main
    bmcweb code doesn't have to understand the details.
    
    Tested:
     - CI passes with new unit tests and audit-events enabled.
     - Confirmed sampling of events have the correct detail data included.
     - Confirmed user session events do not have detail data.
    
    Signed-off-by: Janet Adkins <janeta@us.ibm.com>
    
    * Audit Log: Address code review comments from audit data refactoring
    
    Signed-off-by: Janet Adkins <janeta@us.ibm.com>
    
    * Audit Log: Address code review comments from audit data refactoring
    
    Signed-off-by: Janet Adkins <janeta@us.ibm.com>
    
    * Audit Log: Use std::string for building message buffer
    
    Signed-off-by: Janet Adkins <janeta@us.ibm.com>
    
    ---------
    
    Signed-off-by: Janet Adkins <janeta@us.ibm.com>

--- include/audit_events.hpp
+++ include/audit_events.hpp
@@ -95,22 +95,22 @@ inline void auditSetState(bool enable)
 }
 
 /**
- * @brief Checks if POST request should be audited on completion
+ * @brief Checks if POST request is a user connection event
  *
  * Login and Session requests are audited when the authentication is attempted.
  * This allows failed requests to be audited with the user detail.
  *
- * @return True if request should be audited
+ * @return True if request is a user connection event
  */
-inline bool checkPostAudit(const crow::Request& req)
+inline bool checkPostUser(const crow::Request& req)
 {
     if ((req.target() == "/redfish/v1/SessionService/Sessions") ||
         (req.target() == "/redfish/v1/SessionService/Sessions/") ||
         (req.target() == "/login"))
     {
-        return false;
+        return true;
     }
-    return true;
+    return false;
 }
 
 /**
@@ -123,7 +123,7 @@ inline bool wantAudit(const crow::Request& req)
         (req.method() == boost::beast::http::verb::put) ||
         (req.method() == boost::beast::http::verb::delete_) ||
         ((req.method() == boost::beast::http::verb::post) &&
-         checkPostAudit(req)))
+         !checkPostUser(req)))
     {
         return true;
     }
@@ -131,81 +131,170 @@ inline bool wantAudit(const crow::Request& req)
     return false;
 }
 
-inline void auditEvent(const char* opPath, const std::string& userName,
-                       const std::string& ipAddress, bool success)
+/**
+ * @brief Checks if request should include additional data
+ *
+ * - Accounts requests data may contain passwords
+ * - IBM Management console events data is not useful. It can be binary data or
+ *   contents of file.
+ * - User login and session data may contain passwords
+ *
+ * @return True if request's data should not be logged
+ */
+inline bool checkSkipDetail(const crow::Request& req)
 {
-    int code = __LINE__;
+    if (req.target().starts_with("/redfish/v1/AccountService/Accounts") ||
+        req.target().starts_with("/ibm/v1") ||
+        ((req.method() == boost::beast::http::verb::post) &&
+         checkPostUser(req)))
+    {
+        return true;
+    }
+    return false;
+}
 
-    char cnfgBuff[256];
-    size_t bufLeft = 256; // Amount left available in cnfgBuff
-    char* user = NULL;
-    size_t opPathLen;
-    size_t userLen = 0;
-    int rc;
-    int origErrno;
+/**
+ * @brief Checks if request's detail data should be logged
+ *
+ * @return True if request's detail data should be logged
+ */
+inline bool wantDetail(const crow::Request& req)
+{
+    switch (req.method())
+    {
+        case boost::beast::http::verb::patch:
+        case boost::beast::http::verb::post:
+            if (checkSkipDetail(req))
+            {
+                return false;
+            }
+            return true;
 
-    if (auditOpen() == false)
+        case boost::beast::http::verb::put:
+            return (!req.target().starts_with("/ibm/v1"));
+
+        case boost::beast::http::verb::delete_:
+            return true;
+
+        default:
+            // Shouldn't be here, don't log any data
+            BMCWEB_LOG_DEBUG << "Unexpected verb " << req.method();
+            return false;
+    }
+}
+
+/**
+ * @brief Appends item to strBuf only if strBuf won't exceed maxBufSize
+ *
+ * @param[in,out] strBuf Buffer to append up to maxBufSize only
+ * @param[in] maxBufSize Maximum length of strBuf
+ * @param[in] item String to append if it will fit within maxBufSize
+ *
+ * @return True if item was appended
+ */
+inline bool appendItemToBuf(std::string& strBuf, size_t maxBufSize,
+                            const std::string& item)
+{
+    if (strBuf.length() + item.length() > maxBufSize)
+    {
+        return false;
+    }
+    strBuf += item;
+    return true;
+}
+
+inline void auditEvent(const crow::Request& req, const std::string& userName,
+                       bool success)
+{
+    if (!auditOpen())
     {
         return;
     }
 
-    opPathLen = std::strlen(opPath) + 1;
-    if (opPathLen > bufLeft)
+    std::string opPath = "op=" + std::string(req.methodString()) + ":" +
+                         std::string(req.target()) + " ";
+
+    size_t maxBuf = 256; // Limit message to avoid filling log with single entry
+    std::string cnfgBuff = opPath.substr(0, maxBuf);
+
+    if (cnfgBuff.length() < opPath.length())
     {
-        // Truncate event message to fit into fixed sized buffer.
+        // Event message truncated to fit into fixed sized buffer.
         BMCWEB_LOG_WARNING << "Audit buffer too small, truncating:"
-                           << " bufLeft=" << bufLeft
-                           << " opPathLen=" << opPathLen;
-        opPathLen = bufLeft;
-        code = __LINE__;
+                           << " cnfgBufLen=" << cnfgBuff.length()
+                           << " opPathLen=" << opPath.length();
     }
-    strncpy(cnfgBuff, opPath, opPathLen);
-    cnfgBuff[opPathLen - 1] = '\0';
-    bufLeft -= opPathLen;
+
+    // Determine any additional info for the event
+    std::string detail;
+    if (wantDetail(req))
+    {
+        detail = req.body + " ";
+    }
+
+    if (!detail.empty())
+    {
+        if (!appendItemToBuf(cnfgBuff, maxBuf, detail))
+        {
+            // Additional info won't fix into fixed sized buffer. Leave
+            // it off.
+            BMCWEB_LOG_WARNING << "Audit buffer too small for data:"
+                               << " bufLeft=" << (maxBuf - cnfgBuff.length())
+                               << " detailLen=" << detail.length();
+        }
+    }
 
     // encode user account name to ensure it is in an appropriate format
-    user = audit_encode_nv_string("acct", userName.c_str(), 0);
+    size_t userLen = 0;
+    char* user = audit_encode_nv_string("acct", userName.c_str(), 0);
+
     if (user == NULL)
     {
-        BMCWEB_LOG_ERROR << "Error appending user to audit msg : " << errno;
-        code = __LINE__;
+        BMCWEB_LOG_WARNING << "Error encoding user for audit msg : " << errno;
     }
     else
     {
-        userLen = std::strlen(user) + 1;
+        // setup a unique_ptr to handle freeing memory from user
+        std::unique_ptr<char, void (*)(char*)> userUP(user, [](char* ptr) {
+            if (ptr != nullptr)
+            {
+                ::free(ptr);
+            }
+        });
 
-        if (userLen > bufLeft)
+        userLen = std::strlen(user);
+
+        if (!appendItemToBuf(cnfgBuff, maxBuf, std::string(user)))
         {
             // Username won't fit into fixed sized buffer. Leave it off.
             BMCWEB_LOG_WARNING << "Audit buffer too small for username:"
-                               << " bufLeft=" << bufLeft
+                               << " bufLeft=" << (maxBuf - cnfgBuff.length())
                                << " userLen=" << userLen;
-            code = __LINE__;
-        }
-        else
-        {
-            strncat(cnfgBuff, user, userLen);
         }
     }
 
-    BMCWEB_LOG_DEBUG << "auditEvent: code=" << code << " bufLeft=" << bufLeft
-                     << " opPathLen=" << opPathLen << " userLen=" << userLen;
+    BMCWEB_LOG_DEBUG << "auditEvent: bufLeft=" << (maxBuf - cnfgBuff.length())
+                     << " opPathLen=" << opPath.length()
+                     << " detailLen=" << detail.length()
+                     << " userLen=" << userLen;
 
-    free(user);
+    std::string ipAddress = req.ipAddress.to_string();
 
-    rc = audit_log_user_message(auditfd, AUDIT_USYS_CONFIG, cnfgBuff,
-                                boost::asio::ip::host_name().c_str(),
-                                ipAddress.c_str(), NULL, int(success));
+    int rc = audit_log_user_message(auditfd, AUDIT_USYS_CONFIG,
+                                    cnfgBuff.c_str(),
+                                    boost::asio::ip::host_name().c_str(),
+                                    ipAddress.c_str(), NULL, int(success));
 
     if (rc <= 0)
     {
         // Something failed with existing connection. Try to establish a new
         // connection and retry if successful.
         // Preserve original errno to report if the retry fails.
-        origErrno = errno;
+        int origErrno = errno;
         if (auditReopen())
         {
-            rc = audit_log_user_message(auditfd, AUDIT_USYS_CONFIG, cnfgBuff,
+            rc = audit_log_user_message(auditfd, AUDIT_USYS_CONFIG,
+                                        cnfgBuff.c_str(),
                                         boost::asio::ip::host_name().c_str(),
                                         ipAddress.c_str(), NULL, int(success));
         }
--- test/include/audit_events_test.cpp
+++ test/include/audit_events_test.cpp
@@ -96,5 +96,106 @@ TEST(wantAudit, NegativeTest)
     EXPECT_FALSE(wantAudit(getRequest));
 }
 
+TEST(wantDetail, PositiveTest)
+{
+    constexpr const std::string_view url = "/foo";
+    std::error_code ec;
+
+    crow::Request patchRequest{{boost::beast::http::verb::patch, url, 11}, ec};
+    EXPECT_TRUE(wantDetail(patchRequest));
+
+    crow::Request putRequest{{boost::beast::http::verb::put, url, 11}, ec};
+    EXPECT_TRUE(wantDetail(putRequest));
+
+    crow::Request deleteRequest{{boost::beast::http::verb::delete_, url, 11},
+                                ec};
+    EXPECT_TRUE(wantDetail(deleteRequest));
+
+    crow::Request postRequest{{boost::beast::http::verb::post, url, 11}, ec};
+    EXPECT_TRUE(wantDetail(postRequest));
+}
+
+TEST(wantDetail, NegativeTest)
+{
+    constexpr const std::string_view url = "/foo";
+    std::error_code ec;
+
+    crow::Request patchRequest{{boost::beast::http::verb::patch, url, 11}, ec};
+    patchRequest.target("/redfish/v1/AccountService/Accounts/foo");
+    EXPECT_FALSE(wantDetail(patchRequest));
+
+    patchRequest.target("/ibm/v1/foo");
+    EXPECT_FALSE(wantDetail(patchRequest));
+
+    crow::Request postRequest{{boost::beast::http::verb::post, url, 11}, ec};
+
+    postRequest.target("/redfish/v1/AccountService/Accounts/bar");
+    EXPECT_FALSE(wantDetail(postRequest));
+
+    postRequest.target("/ibm/v1/bar");
+    EXPECT_FALSE(wantDetail(postRequest));
+
+    postRequest.target("/redfish/v1/SessionService/Sessions");
+    EXPECT_FALSE(wantDetail(postRequest));
+
+    postRequest.target("/redfish/v1/SessionService/Sessions/");
+    EXPECT_FALSE(wantDetail(postRequest));
+
+    postRequest.target("/login");
+    EXPECT_FALSE(wantDetail(postRequest));
+
+    crow::Request getRequest{{boost::beast::http::verb::get, url, 11}, ec};
+    EXPECT_FALSE(wantDetail(getRequest));
+}
+
+TEST(appendItemToBuf, PositiveTest)
+{
+    std::string testBuf;
+    std::string data;
+    std::string cmpStr;
+
+    data = "Initial data ";
+    cmpStr = data;
+
+    EXPECT_TRUE(appendItemToBuf(testBuf, data.length() + 20, data));
+    EXPECT_EQ(testBuf, cmpStr);
+
+    data = "Append data ";
+    cmpStr += data;
+    EXPECT_TRUE(
+        appendItemToBuf(testBuf, testBuf.length() + data.length(), data));
+    EXPECT_EQ(testBuf, cmpStr);
+
+    data = "Append more data ";
+    cmpStr += data;
+
+    EXPECT_TRUE(
+        appendItemToBuf(testBuf, testBuf.length() + data.length(), data));
+    EXPECT_EQ(testBuf, cmpStr);
+}
+
+TEST(appendItemToBuf, NegativeTest)
+{
+    std::string testBuf;
+    std::string data;
+    std::string cmpStr;
+
+    cmpStr = testBuf;
+    data = "Data not appended";
+
+    EXPECT_FALSE(appendItemToBuf(testBuf, data.length() - 5, data));
+    EXPECT_EQ(testBuf, cmpStr);
+
+    testBuf = "Initial data ";
+    cmpStr = testBuf;
+
+    EXPECT_FALSE(appendItemToBuf(testBuf, testBuf.length(), data));
+    EXPECT_EQ(testBuf, cmpStr);
+
+    EXPECT_FALSE(
+        appendItemToBuf(testBuf, testBuf.length() + data.length() - 1, data));
+    EXPECT_EQ(testBuf, cmpStr);
+}
+
 } // namespace
 } // namespace audit
