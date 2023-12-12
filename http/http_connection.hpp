1d8e37ae6087f6a67a91724e6c5b621d0019bf70
Gunnar Mills <gmills@us.ibm.com>
2023-12-12 15:17:06 -0700

    Up the max requests in flight to 200 (#778)
    Have seen with defects 581366 and 580644 hitting the max connection
    limit after multiple HMCs added, etc.
    
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- http/http_connection.hpp
+++ http/http_connection.hpp
@@ -291,7 +291,7 @@ class Connection :
 
     void start()
     {
-        if (connectionCount >= 100)
+        if (connectionCount >= 200)
         {
             BMCWEB_LOG_CRITICAL << this << "Max connection count exceeded.";
             return;
