86256f4e49b156e0ce975c4ba7420ed20f260c81
George Liu <liuxiwei@ieisystem.com>
2024-02-23 22:36:59 +0800

    Fix bug: bmcweb SNMP errors in the Journal
    Fix this bug: ibm-openbmc/dev#3599
    Don't send event to snmptrap subscription.
    
    Signed-off-by: Abiola Asojo <abiola.asojo@ibm.com>

--- redfish-core/src/subscription.cpp
+++ redfish-core/src/subscription.cpp
@@ -181,6 +181,11 @@ void Subscription::onHbTimeout(const std::weak_ptr<Subscription>& weakSelf,
 
 bool Subscription::sendEventToSubscriber(uint64_t eventId, std::string&& msg)
 {
+    if (userSub->subscriptionType == "SNMPTrap")
+    {
+        return false; // Don't need send SNMPTrap event.
+    }
+
     persistent_data::EventServiceConfig eventServiceConfig =
         persistent_data::EventServiceStore::getInstance()
             .getEventServiceConfig();
