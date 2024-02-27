3c91d083e9ae083a3bcc127c6e5fa495292361f3
Myung Bae <myungbae@us.ibm.com>
2024-02-27 08:29:25 -0500

    Fix coredump on async method during validatePrivilege
    PATCH may cause bmcweb to coredump depending on timing
    of  `validatePrivilege` execution. It is because `req' is captured as
    reference, and it may be cleared-up before async-call method completes.
    (This problem can be seen more frequently by enabling debug mode).
    
    This commit is to keep `req` during to async-method execution.
    
    Tested:
    
    - Create a ReadOnly user - here, called as `readonly`
    
    - Using `redfishtool`, run PATCH on `readonly` user role.
    
    ```
    $ redfishtool  -vvvvv raw -r ${bmc}:18080 -u ${user} -p ${password} -S Always PATCH /redfish/v1/AccountService/Accounts/readonly --data='{"RoleId":"Administrator"}'
    ...
    This sometimes fails because bmcweb coredump
    ```
    
    After:
    
    ```
    $ redfishtool  raw -r ${bmc}:18080 -u ${user} -p ${password} -S Always PATCH /redfish/v1/AccountService/Accounts/readonly --data='{"RoleId":"Administrator"}'
    {
        "@odata.id": "/redfish/v1/AccountService/Accounts/readonly",
        "@odata.type": "#ManagerAccount.v1_7_0.ManagerAccount",
        ...
    }
    ```
    
    Change-Id: I2a28d1743cfc0fbd9239f69dec5584b34c7ebe43
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

--- http/routing.hpp
+++ http/routing.hpp
@@ -1460,7 +1460,7 @@ class Router
         std::string username = req.session->username;
 
         crow::connections::systemBus->async_method_call(
-            [&req, asyncResp, &rule, params](
+            [req{std::move(req)}, asyncResp, &rule, params](
                 const boost::system::error_code ec,
                 const dbus::utility::DBusPropertiesMap& userInfoMap) mutable {
             if (ec)
