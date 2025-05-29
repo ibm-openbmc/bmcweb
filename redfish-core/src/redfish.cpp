fa434cc96d36321717d6ef354634d042d53582dd
Asmitha Karunanithi <47350623+asmithakarun@users.noreply.github.com>
2025-05-29 20:59:59 +0530

    Add BIOS support (#907) (#1298)
    * Add Attributes property under Bios routes
    
    The patch adds `Attributes` property under
    `redfish/v1/Systems/{}/Bios`. The Attributes are fetched from
    `bios-config-manager`(https://github.com/openbmc/bios-settings-mgr).
    `bios-settings-manager` exposes a property `BaseBIOSTable` which is used
    to fill Attributes field.
    
    The redfish schema exposing the property  is:
    https://redfish.dmtf.org/schemas/v1/Bios.v1_2_3.json
    
    Tested:
    1) Redfish service validator test is successful
    2) Tested on romulus qemu transferring biosData file
       and the test is successful
    
    Change-Id: I7f05949420e468ce406dd26a86a51c0dc264eccf
    Signed-off-by: Prithvi Pai <ppai@nvidia.com>
    
    * Add BiosAttributeRegistry API & Bios settings API
    
    This commit adds BiosAttributeRegistry redfish endpoint that has the
    metadata of the bios attributes supported by the system.
    
    It also adds Bios settings API that the user can use to patch/update the
    bios attribute values.
    
    Tested By:
    [1] GET https://<bmc_ip>/redfish/v1/Registries/BiosAttributeRegistry
    [2] Single/Multiple Attributes:
    '''
    PATCH '{"Attributes":{"":""}}' https://${bmc}/redfish/v1/Systems/system/Bios/Settings
    '''
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    
    ---------
    
    Signed-off-by: Prithvi Pai <ppai@nvidia.com>
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Co-authored-by: Prithvi Pai <ppai@nvidia.com>

--- redfish-core/src/redfish.cpp
+++ redfish-core/src/redfish.cpp
@@ -179,6 +179,7 @@ RedfishService::RedfishService(App& app)
     requestRoutesSystemActionsOemExecutePanelFunction(app);
 
     requestRoutesBiosService(app);
+    requestRoutesBiosSettings(app);
     requestRoutesBiosReset(app);
 
     if constexpr (BMCWEB_VM_NBDPROXY)
