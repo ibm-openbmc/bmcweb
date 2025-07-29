665a9bd5c7d0c96fe73af5bfba04cad1513996f7
Abiola Asojo <abiola.asojo@ibm.com>
2025-07-29 19:00:04 +0000

    1120 Fix 204/304 incorrectly tracing CRITICAL message
    Resolve tracing of CRITICAL message on 204(no-content) and 304(not
    modified) when there is no body in the response.
    Removed setting of no_content on PATCH that was being done before other
    functions are called.
    
    The code is changed to check that 204 and 304 response with payloadsize
    of 0 will not trace the CRITICAL message.
    
    Tested: After running the following commands, the CRITICAL message is no
    longer being traced for no-content and not modified response with no
    body.
    
    204 no-content
    ```
      curl -k -H "X-Auth-Token: $bmc_token" -H "Content-Type: application/json" -X PATCH https://${bmc}/redfish/v1/Managers/bmc -d '{"Oem":{"IBM":{"USBCodeUpdateEnabled": false}}}'
    
      curl -k -H "Content-Type: application/json" -d '{"PowerRestorePolicy":"LastState"}' -X PATCH https://${bmc}/redfish/v1/Systems/system
    
      curl -k -H "Content-Type: application/json" -H "X-Auth-Token: $bmc_token" -X PATCH -d '{"LocationIndicatorActive":true}' https://${bmc}/redfish/v1/Managers/bmc
    ```
    
    304 not-modified
    ```
      curl -k -i -H 'If-Modified-Since: Tue, 21 Nov 2050 08:00:00 GMT' https://${bmc_ip}/redfish/ --etag-save etag.out
    
      HTTP/1.1 200 OK
      Allow: GET
      OData-Version: 4.0
      Strict-Transport-Security: max-age=31536000; includeSubdomains
      Pragma: no-cache
      Cache-Control: no-store, max-age=0
      X-Content-Type-Options: nosniff
      ETag: "B3A9EAA1"
      Content-Type: application/json
      Date: Thu, 14 Aug 2025 17:53:03 GMT
      Content-Length: 26
    
      ETAG=`cat etag.out`;  echo $ETAG
    
      curl -k -i -H "If-None-Match: ${ETAG}" https://${bmc_ip}/redfish/
    
      HTTP/1.1 304 Not Modified
      Allow: GET
      OData-Version: 4.0
      Strict-Transport-Security: max-age=31536000; includeSubdomains
      Pragma: no-cache
      Cache-Control: no-store, max-age=0
      X-Content-Type-Options: nosniff
      ETag: "B3A9EAA1"
      Date: Thu, 14 Aug 2025 17:53:57 GMT
      Content-Length: 0
    ```
    
    Signed-off-by: Abiola Asojo <abiola.asojo@ibm.com>

--- redfish-core/lib/systems.hpp
+++ redfish-core/lib/systems.hpp
@@ -36,7 +36,6 @@
 
 #include <boost/asio/error.hpp>
 #include <boost/beast/http/field.hpp>
-#include <boost/beast/http/status.hpp>
 #include <boost/beast/http/verb.hpp>
 #include <boost/system/error_code.hpp>
 #include <boost/system/linux_error.hpp>
@@ -2970,8 +2969,6 @@ inline void handleComputerSystemPatch(
             return;
         }
     }
-
-    asyncResp->res.result(boost::beast::http::status::no_content);
 
     if (assetTag)
     {
